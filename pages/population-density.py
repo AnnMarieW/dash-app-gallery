7b783c8dfc7e2537eefef4d49d05713a2d4b88e7
Tolgahan Cepel <tolgahan.cepel@gmail.com>
2022-04-15 22:36:23 +0300

    added categories

--- pages/population-density.py
+++ pages/population-density.py
@@ -3,7 +3,13 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Dash Sample App")
+dash.register_page(
+    __name__,
+    description = "Dash Sample App",
+    layout_type = "1 Row 1 Col",
+    graph = "Choropleth",
+    # callback = "None"
+)
 
 filename = __name__.split("pages.")[1]
 
