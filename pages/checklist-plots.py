4159021a4ebf2cab457ea9f8e506eef45fc2cacf
Roberto Panai <robertopanai@sardus.it>
2022-04-21 21:46:58 -0400

    Update checklist-plots.py

--- pages/checklist-plots.py
+++ pages/checklist-plots.py
@@ -3,7 +3,7 @@ from utils.code_and_show import example_app
 dash.register_page(
 ￼    __name__,
 ￼    description = "Dash Sample App",
-￼    layout_type = "1 Row 1 Col",
+￼    layout_type = "2Row():1Col()",
 ￼    graph = "scatter",
     components = "checklist",
 ￼    callback = "1input:1output",
