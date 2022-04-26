672787aa164ba4f46966383d7d4c39d17a4c6c38
Charming Data <info@charmingdata.org>
2022-04-26 09:39:23 -0400

    Update checklist-plots.py

--- pages/examples/checklist-plots.py
+++ pages/examples/checklist-plots.py
@@ -12,45 +12,28 @@ options = [
     {'label': specie.capitalize(),
      'value': specie} for specie in species]
 
-fig = px.scatter(
-        df,
-        x="sepal_length",
-        y="sepal_width", 
-        color="species"
-)
-
-color_discrete_map = {d.name: d.marker.color for d in fig.data}
-
-range_x = df["sepal_length"].agg({"min", "max"})
-range_y = df["sepal_width"].agg({"min", "max"})
-range_x = [range_x["min"]*.9, range_x["max"]*1.1]
-range_y = [range_y["min"]*.9, range_y["max"]*1.1]
-
 # App
 app = Dash(__name__)
 
 app.layout = html.Div([
     dcc.Checklist(
         options=options,
-        labelStyle={'display': 'inline-block'},
+        inline=True,
         value=species,
-        id='checklist-plots-checklist'),
-     dcc.Graph(id='checklist-plots-scatter')
+        id='checklist-plots-x-checklist'),
+     dcc.Graph(id='checklist-plots-x-scatter')
 ])
 
 @app.callback(
-    Output('checklist-plots-scatter', 'figure'),
-    Input('checklist-plots-checklist', 'value'))
+    Output('checklist-plots-x-scatter', 'figure'),
+    Input('checklist-plots-x-checklist', 'value'))
 
 def update_figure(values):
     fig = px.scatter(
         df[df["species"].isin(values)],
         x="sepal_length",
-        y="sepal_width", 
+        y="sepal_width",
         color="species",
-        color_discrete_map=color_discrete_map,
-        range_x=range_x,
-        range_y=range_y
     )
     return fig
 
