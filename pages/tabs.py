1fff342ad7cf00bb311ccf07407c31ae58429fa8
Charming Data <info@charmingdata.org>
2022-04-26 09:53:16 -0400

    Update tabs.py

--- pages/tabs.py
+++ pages/tabs.py
@@ -1,13 +1,18 @@
+import dash
 from utils.code_and_show import example_app
 
 dash.register_page(
-￼    __name__,
-￼    description = "Dash Sample App",
-￼    layout_type = "1 Row 1 Col",
-￼    graph = "scatter",
-    components = "tabs",
-    dataset = "iris, gapminder"
-￼    # callback = "None"
-￼)
-￼
-￼filename = __name__.split("pages.")[1]
+    __name__,
+    description = "Two tabs that plot the Iris and Gapminder data on a bubble chart and scatter graph",
+    layout_type = "tabs",
+    components_type  = ["tabs"],
+    graph_type = ["scatter", "bubble"],
+    callback_type  = "None",
+)
+
+filename = __name__.split("pages.")[1]
+
+def layout():
+    return example_app(f"pages/examples/{filename}.py")
+
+
