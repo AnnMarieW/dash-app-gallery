64f62fabea4123bea64257c79addb1fb31de2ecc
Somesh <42097653+someshfengde@users.noreply.github.com>
2022-05-04 00:57:52 +0530

    Update pages/stopwatch-timer.py
    cleaner code
    
    Co-authored-by: Charming Data <info@charmingdata.org>

--- pages/stopwatch-timer.py
+++ pages/stopwatch-timer.py
@@ -3,8 +3,23 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="stopwatch timer application with 2 buttons ")
+dash.register_page(
+    __name__,
+    description = "Stopwatch timer application with 1 button and an input field.",
+    layout_type = "general",
+    components_type = ["leddisplay", "input", "interval", "powerbutton"],
+    graph_type = "none", 
+    callback_type = "general"
+)
+
+filename = __name__.split("pages.")[1]
+
+# make a mini layout with the links to the docs that would go under each app
+# all_links: {"input": "https://dash.plotly.com/dash-core-components/input",
+# "interval": "https://dash.plotly.com/dash-core-components/interval",
+# "dash-daq": "https://dash.plotly.com/dash-daq"} 
+# links = html.Div([all_links])
 
 
 def layout():
-    return example_app(f"pages/examples/stopwatch.py")
+    return example_app(f"pages/examples/{filename}.py")
