cf2afc548c8ce7aa560d0f5573bd5ee9339d2397
Charming Data <info@charmingdata.org>
2022-05-03 17:27:23 -0400

    Update poisson-distribution.py

--- pages/poisson-distribution.py
+++ pages/poisson-distribution.py
@@ -5,9 +5,9 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description = "This app plots poisson distribution on a histogram, contains a formula with MathJax.",
-    layout_type = "3row:1col",
-    components_type = ["input", "card"],
+    description = "This app plots a Poisson distribution on a histogram, which contains a formula with MathJax.",
+    layout_type = "cards",
+    components_type = ["input", "card", "accordion", "markdown"],
     graph_type = "histogram", 
     callback_type = "general"
 )
@@ -15,7 +15,12 @@ dash.register_page(
 filename = __name__.split("pages.")[1]
 
 # make a mini layout with the links to the docs that would go under each app
-# links = html.Div([])
+# all_links: {"input": "https://dash.plotly.com/dash-core-components/input",
+# "card": "https://dash-bootstrap-components.opensource.faculty.ai/docs/components/card/",
+# "accordion": "https://dash-bootstrap-components.opensource.faculty.ai/docs/components/accordion/",
+# "markdown": "https://dash.plotly.com/dash-core-components/markdown",
+# "histogram": "https://plotly.com/python/histograms/"} 
+# links = html.Div([all_links])
 
 def layout():
     return example_app(f"pages/examples/{filename}.py")
