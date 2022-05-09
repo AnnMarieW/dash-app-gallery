39fe3bcef26caa2f835141d8e641dc4c8a894202
AnnMarieW <amward@fastmail.com>
2022-05-09 13:18:56 -0700

    Added notes section

--- pages/poisson-distribution.py
+++ pages/poisson-distribution.py
@@ -1,6 +1,8 @@
 import dash
+from dash import html, dcc
 
 from utils.code_and_show import example_app
+
 
 
 dash.register_page(
@@ -8,8 +10,8 @@ dash.register_page(
     description = "This app plots a Poisson distribution on a histogram, which contains a formula with MathJax.",
     layout_type = "cards",
     components_type = ["input", "card", "accordion", "markdown"],
-    graph_type = "histogram", 
-    callback_type = "general"
+    graph_type = "histogram",
+    callback_type = "general",
 )
 
 filename = __name__.split("pages.")[1]
@@ -19,8 +21,36 @@ filename = __name__.split("pages.")[1]
 # "card": "https://dash-bootstrap-components.opensource.faculty.ai/docs/components/card/",
 # "accordion": "https://dash-bootstrap-components.opensource.faculty.ai/docs/components/accordion/",
 # "markdown": "https://dash.plotly.com/dash-core-components/markdown",
-# "histogram": "https://plotly.com/python/histograms/"} 
+# "histogram": "https://plotly.com/python/histograms/"}
 # links = html.Div([all_links])
 
+
+# any notes will be displayed below the code-and-show page in a dcc.Markdown component
+notes = """
+### For more information see:
+Dash docs:  
+
+- [Input component](https://dash.plotly.com/dash-core-components/input)
+- [Markdown component](https://dash.plotly.com/dash-core-components/markdown)
+
+Plotly docs:  
+
+- [histogram](https://plotly.com/python/histograms/) 
+
+Dash Bootstrap Components docs:
+- [Card](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/card/)
+- [Accordion](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/accordion/)
+
+
+### Contributed by:
+This example app was contributed by [name](link)
+
+"""
+
 def layout():
-    return example_app(f"pages/examples/{filename}.py")
+    return html.Div(
+        [
+            example_app(f"pages/examples/{filename}.py"),
+            dcc.Markdown(notes, className="m-4")
+        ]
+    )
