83e023759e7c167a819ff73a2e1ebd0a87c76aa9
Charming Data <info@charmingdata.org>
2022-05-12 11:30:58 -0400

    Create parallel-coord.py

---
+++ pages/parallel-coord.py
@@ -0,0 +1,40 @@
+import dash
+from dash import html, dcc
+
+from utils.code_and_show import example_app
+
+
+dash.register_page(
+    __name__,
+    description = "This app connects between selected rows in a Dash DataTable to update traces of a parallel coordinates graph.",
+    layout_type = "top-bottom",
+    components_type = ["datatable"],
+    graph_type = "parallel coordinates",
+    callback_type = "general",
+)
+
+filename = __name__.split("pages.")[1]
+
+# any notes will be displayed below the code-and-show page in a dcc.Markdown component
+notes = """
+### For more information see:
+Dash docs:  
+
+- [DataTable component](https://dash.plotly.com/datatable)
+
+Plotly docs:  
+
+- [parallel coordinates](https://plotly.com/python/parallel-coordinates-plot/) 
+
+### Contributed by:
+This example app was contributed by [IcToxi](https://github.com/IcToxi)
+
+"""
+
+def layout():
+    return html.Div(
+        [
+            example_app(filename),
+            dcc.Markdown(notes, className="m-4")
+        ]
+    )
