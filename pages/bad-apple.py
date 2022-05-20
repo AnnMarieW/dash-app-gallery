063acd3740a0e9003d702c5a9f87716fa9f5bf43
adam schroeder <adamschroeder.m@gmail.com>
2022-05-20 00:03:27 -0400

    add bad-apple app

---
+++ pages/bad-apple.py
@@ -0,0 +1,41 @@
+import dash
+from dash import html, dcc
+
+from utils.code_and_show import example_app
+
+
+dash.register_page(
+    __name__,
+    description = "An app that uses clientside callback to display frames because dcc Interval is activated multiple times per second.",
+    layout_type = "top-bottom",
+    components_type = ["store", "interval"],
+    graph_type = None,
+    callback_type = "clientside",
+)
+
+filename = __name__.split("pages.")[1]
+
+# any notes will be displayed below the code-and-show page in a dcc.Markdown component
+notes = """
+### For more information see:
+Dash docs:  
+
+- [Interval component](https://dash.plotly.com/dash-core-components/interval)
+
+- [Store component](https://dash.plotly.com/dash-core-components/store)
+
+- [Clientside Callback](https://dash.plotly.com/clientside-callbacks)
+
+
+### Contributed by:
+This example app was contributed by [IcToxi](https://github.com/IcToxi)
+
+"""
+
+def layout():
+    return html.Div(
+        [
+            example_app(filename),
+            dcc.Markdown(notes, className="m-4")
+        ]
+    )
