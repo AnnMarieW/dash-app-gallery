9859e44e0e495b4437a59ec46eae8b92b5a32b60
Charming Data <info@charmingdata.org>
2022-05-28 21:56:24 -0400

    Update age-alert.py

--- pages/age-alert.py
+++ pages/age-alert.py
@@ -1,25 +1,32 @@
 import dash
 from dash import html, dcc
 
-from utils.code_and_show import example_app, make_app_first
+from utils.code_and_show import example_app
 
 
 dash.register_page(
     __name__,
-    description="application showing usage for Datepicker and alert.",
+    description="This application shows the connections between a Datepicker and Alert.",
     layout_type="top-bottom",
+    components_type=["markdown", "datepicker", "alert", "button"],
     graph_type=None,
-    callback_type="general",
+    callback_type="general"
 )
 
 filename = __name__.split("pages.")[1]
 
 # any notes will be displayed below the code-and-show page in a dcc.Markdown component
 notes = """
-### This application uses dash-bootstrap-components and dash-mantine-components
-components used : 
-    dbc.Alert()
-    dmc.DatePicker()
+#### Dash Components in App:
+- [Markdown component](https://dash.plotly.com/dash-core-components/markdown)
+
+#### 3rd-party Dash Bootstrap Components: 
+- [Button component](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/button/ "This component was made by the community and not officially maintained by Plotly.")
+- [Alert component](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/alert/ "This component was made by the community and not officially maintained by Plotly.")
+
+#### 3rd-party Dash Mantine Components: 
+- [DatePicker component](https://www.dash-mantine-components.com/components/datepicker/ "This component was made by the community and not officially maintained by Plotly.")
+
 
 ### Contributed by:
 This example app was contributed by [someshfengde](https://github.com/someshfengde)
