95b871c81b17bab5924b210ac3867281d8f4a489
Charming Data <info@charmingdata.org>
2022-05-28 22:23:32 -0400

    Update age-alert.py

--- examples/age-alert.py
+++ examples/age-alert.py
@@ -1,6 +1,5 @@
-from dash import Dash, html, dcc, Input, Output, State, callback
+from dash import Dash, html, dcc, Input, Output, callback
 import dash_bootstrap_components as dbc
-import dash_mantine_components as dmc 
 from datetime import datetime, date
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -12,13 +11,12 @@ app.layout = dbc.Container(
         here we are asking user to enter birthdate with datepicker and showing success message when user selects date.
         we will be using duration to make sure alert will last for certain duration
         """), 
-        dmc.DatePicker(
+        dcc.DatePickerSingle(
+            min_date_allowed=date(1900, 8, 5),
+            max_date_allowed=date(2022, 9, 19),
             id="age-alert-x-date-pick",
-            label="Birthday!",
-            description="please pick your birthdate!",
-            minDate=date(1900, 8, 5),
-            maxDate=date(2022, 9, 19),
-            value=date(2002, 9, 19),
+            date=date(2002, 9, 19),
+            placeholder="please pick your birthdate!",
         ),
         html.Div(id = "age-alert-x-show-succ"),
         html.Div(id= "age-alert-x-show-age"),
@@ -43,7 +41,7 @@ app.layout = dbc.Container(
 @callback(
     Output("age-alert-x-show-succ", "children"),
     Output("age-alert-x-show-age", "children"),
-    Input("age-alert-x-date-pick", "value"),
+    Input("age-alert-x-date-pick", "date"),
 prevent_initial_call=True
 )
 def update_output(d): 
@@ -65,7 +63,7 @@ def update_output(d):
     ]
 
 # app callback for toggling alert with button
-@app.callback(
+@callback(
     Output("age-alert-x-fade", "children"),
     [Input("age-alert-x-toggle-fade", "n_clicks")],
 )
