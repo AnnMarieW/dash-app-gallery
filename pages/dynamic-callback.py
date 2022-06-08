f5f92fc5d28daae3dcc131c411694c692b31c93d
Charming Data <info@charmingdata.org>
2022-06-07 23:50:27 -0400

    Update and rename dynamic_callback.py to dynamic-callback.py

---
+++ pages/dynamic-callback.py
@@ -0,0 +1,31 @@
+import dash
+
+from utils.code_and_show import example_app
+
+
+dash.register_page(
+    __name__,
+    description="This application shows how to use chained callbacks with a line graph.",
+    layout_type="top-bottom",
+    components_type=["dropdown"],
+    graph_type="line",
+    callback_type="chained",
+)
+
+filename = __name__.split("pages.")[1]
+
+# any notes will be displayed below the code-and-show page in a dcc.Markdown component
+notes = """
+#### Dash Components in App:
+- [Dropdown component](https://dash.plotly.com/dash-core-components/dropdown)
+- [Chained Callback](https://dash.plotly.com/basic-callbacks#dash-app-with-chained-callbacks)
+
+#### Plotly Documentation:
+- [Line chart](https://plotly.com/python/line-charts/)
+
+##### Contributed by:
+This example app was contributed by [Milan](https://milanzmitrovic.github.io/)
+
+"""
+
+layout = example_app(filename, notes=notes)
