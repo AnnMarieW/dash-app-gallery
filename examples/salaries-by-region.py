df5748a075490fc9d072634c18785390b6e17389
Charming Data <info@charmingdata.org>
2022-06-08 00:18:44 -0400

    used black to edit code

--- examples/salaries-by-region.py
+++ examples/salaries-by-region.py
@@ -3,22 +3,39 @@ import plotly.express as px
 import pandas as pd
 import dash_bootstrap_components as dbc
 
-lst = [['North', 'Jan', 500], ['North', 'Feb', 1500], ['North', 'Mar', 2000], ['North', 'Apr', 2500], ['North', 'May', 3000],
-       ['East', 'Jan', 3000], ['East', 'Feb', 2000], ['East', 'Mar', 1000], ['East', 'Apr', 4000], ['East', 'May', 4500],
-       ['West', 'Jan', 1200], ['West', 'Feb', 1400], ['West', 'Mar', 1600], ['West', 'Apr', 1800], ['West', 'May', 2200],
-       ['South', 'Jan', 700], ['South', 'Feb', 900], ['South', 'Mar', 400], ['South', 'Apr', 600], ['South', 'May', 800]
-      ]
-df = pd.DataFrame(lst, columns =["Region", "Month", "Salary"])
+lst = [
+    ["North", "Jan", 500],
+    ["North", "Feb", 1500],
+    ["North", "Mar", 2000],
+    ["North", "Apr", 2500],
+    ["North", "May", 3000],
+    ["East", "Jan", 3000],
+    ["East", "Feb", 2000],
+    ["East", "Mar", 1000],
+    ["East", "Apr", 4000],
+    ["East", "May", 4500],
+    ["West", "Jan", 1200],
+    ["West", "Feb", 1400],
+    ["West", "Mar", 1600],
+    ["West", "Apr", 1800],
+    ["West", "May", 2200],
+    ["South", "Jan", 700],
+    ["South", "Feb", 900],
+    ["South", "Mar", 400],
+    ["South", "Apr", 600],
+    ["South", "May", 800],
+]
+df = pd.DataFrame(lst, columns=["Region", "Month", "Salary"])
 
-# Defining the Dash application 
+# Defining the Dash application
 app = Dash(external_stylesheets=[dbc.themes.DARKLY])
 
 # Dropdown menu
 dropdown_menu = dcc.Dropdown(
-    id="salaries-by-region-dropdown",
+    id="salaries-by-region-x-dropdown",
     options=["North", "East", "West", "South"],
     value=["North", "East", "West", "South"],
-    multi=True
+    multi=True,
 )
 
 # Application layout
@@ -29,16 +46,16 @@ app.layout = dbc.Container(
         dbc.Row(
             [
                 dbc.Col(dropdown_menu, lg=6, sm=6, style={"color": "black"}),
-                dbc.Col(dcc.Graph(id="salaries-by-region-graph"), lg=6, sm=6)
+                dbc.Col(dcc.Graph(id="salaries-by-region-x-graph"), lg=6, sm=6),
             ]
-       )
+        ),
     ]
 )
 
 # Callback
 @app.callback(
-    Output("salaries-by-region-graph", "figure"),
-    Input("salaries-by-region-dropdown", "value")
+    Output("salaries-by-region-x-graph", "figure"),
+    Input("salaries-by-region-x-dropdown", "value"),
 )
 def make_line_graph(region_list):
 
@@ -46,17 +63,14 @@ def make_line_graph(region_list):
 
     fig = px.line_3d(dff, x="Region", y="Month", z="Salary", color="Region")
 
-    fig.update_traces(
-        line_width=13
-    )
+    fig.update_traces(line_width=13)
 
     fig.update_layout(
-        plot_bgcolor="#222222",
-        paper_bgcolor="#222222",
-        font_color="white"
+        plot_bgcolor="#222222", paper_bgcolor="#222222", font_color="white"
     )
 
     return fig
+
 
 # Run the app
 if __name__ == "__main__":
