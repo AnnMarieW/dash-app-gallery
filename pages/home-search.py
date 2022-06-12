9ee62d65fad6a184a483b5ac9e01fff6deafa86f
AnnMarieW <amward@fastmail.com>
2022-06-12 14:55:04 -0700

    added featured apps

--- pages/home-search.py
+++ pages/home-search.py
@@ -1,37 +1,15 @@
 import dash
-from dash import html, dcc, callback, Output, Input, State, callback_context as ctx
+from dash import html, callback, Output, Input, ALL, ctx
 import dash_bootstrap_components as dbc
 from utils.search import search_code_files
-from utils.card_grid import make_card, make_card_grid
+from utils.card_grid import make_card_grid
+from utils.feature_app import feature_app_div
 
 
 dash.register_page(
     __name__, name="Home - Overview", description="Dash App Gallery", path="/"
 )
 
-
-callback_structure_div = html.Div(
-    [
-        "Select Callback Structure:",
-        dcc.Dropdown(
-            ["All", "1 Output 1 Input", "1 Output 2 Input"],
-            "All",
-            clearable=False,
-            id="home-search-x-dd",
-            className="mb-4",
-            persistence=True,
-        ),
-    ]
-)
-
-graph_types_div = html.Div(
-    [
-        "Select Graph Type:",
-        dcc.Dropdown(
-            ["All", "To Do"], "All", clearable=False, className="mb-4", persistence=True
-        ),
-    ]
-)
 
 case_sensitive = (
     dbc.Switch(
@@ -63,46 +41,56 @@ search_code_div = html.Div(
 )
 
 
+def filtered_registry(filtered_example_app_list):
+    """
+    Returns a filtered dash.page_registry dict based on a list of example app names
+    """
+
+    # We use the module param to filter the dash_page_registry
+    # Note that the module name includes the pages folder name eg: "pages.bar-charts"
+    filtered_registry = []
+    for page in dash.page_registry.values():
+        filename = page["module"].split("pages.")[1]
+        if filename in filtered_example_app_list:
+            filtered_registry.append(page)
+    return filtered_registry
+
+
 layout = html.Div(
     [
-        dbc.Row([dbc.Col(search_code_div), dbc.Col(callback_structure_div)]),
+        dbc.Row([dbc.Col(search_code_div), dbc.Col(feature_app_div)]),
         dbc.Row(dbc.Col(html.Div(id="home-search-x-grid"))),
-    ], className="p-4 mx-5"
+    ],
+    className="p-4 mx-5",
 )
 
 
 @callback(
     Output("home-search-x-grid", "children"),
     Output("home-search-x-code-search-input", "value"),
-    Output("home-search-x-dd", "value"),
     Input("home-search-x-code-search-input", "value"),
-    Input("home-search-x-dd", "value"),
     Input("home-search-x-case-sensitive", "value"),
+    Input({"type": "feature_app", "index": ALL}, "n_clicks"),
 )
-def update(searchterms, dd_value, case_sensitive):
-    input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+def update(searchterms, case_sensitive, feature_app):
+    input_id = ctx.triggered_id
     registry = dash.page_registry.values()
 
+    # show apps based on search field
     if input_id == "home-search-x-code-search-input":
         if searchterms:
             filtered_example_app_names = search_code_files(searchterms, case_sensitive)
+            registry = filtered_registry(filtered_example_app_names)
+            return make_card_grid(registry=registry), dash.no_update
 
-            # We use the module param to filter the dash_page_registry
-            # Note that the module name includes the pages folder name eg: "pages.bar-charts"
-            registry = []
-            for page in dash.page_registry.values():
-                filename = page["module"].split("pages.")[1]
-                if filename in filtered_example_app_names:
-                    registry.append(page)
-            return make_card_grid(registry=registry), dash.no_update, "All"
+    # show feature apps
+    if isinstance(input_id, dict):
+        # The searchterms are from the "index" key of the pattern matching dict id
+        # See `utils.feature_app.py for more details.
+        searchterms = input_id["index"]
+        case_sensitive = True
+        filtered_example_app_names = search_code_files(searchterms, case_sensitive)
+        registry = filtered_registry(filtered_example_app_names)
+        return make_card_grid(registry=registry), None
 
-    if input_id == "home-search-x-dd":
-        if dd_value != "All":
-            registry = [
-                p
-                for p in dash.page_registry.values()
-                if p.get("callback_dd") == dd_value
-            ]
-            return make_card_grid(registry=registry), None, dash.no_update
-
-    return make_card_grid(registry=registry), None, "All"
+    return make_card_grid(registry=registry), None
