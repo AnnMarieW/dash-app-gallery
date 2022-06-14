15cac4a095638efef891cc84be489931694b26de
Tolgahan <39929063+tolgahancepel@users.noreply.github.com>
2022-06-14 05:31:09 +0300

    country-distances app (#49)
    * country distances initial commit
    
    * comments, libraries and layout adjustments
    
    * black executed
    
    * second figure and screenshot update
    
    * screenshot rotated

---
+++ examples/country-distances.py
@@ -0,0 +1,83 @@
+from dash import Dash, dcc, html, Input, Output, no_update
+import plotly.express as px
+import dash_bootstrap_components as dbc
+
+# Defining the Dash application
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+df = px.data.gapminder().query("year == 2007")
+
+dropdown_country_a = dcc.Dropdown(
+    id="country-distances-dropdown-a",
+    options=[cntry for cntry in df.country],
+    value="Turkey",
+)
+
+dropdown_country_b = dcc.Dropdown(id="country-distances-dropdown-b")
+
+info_card = dbc.Card(dbc.CardBody(html.P("Choose two countries and drag to rotate.")))
+
+app.layout = dbc.Container(
+    [
+        html.H2("Country distances on map"),
+        html.Hr(),
+        dbc.Row(
+            [
+                dbc.Col([dropdown_country_a, dropdown_country_b], lg=6, sm=12),
+                dbc.Col(info_card, lg=6, sm=12),
+            ]
+        ),
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id="country-distances-locations-graph"), lg=6, sm=12),
+                dbc.Col(dcc.Graph(id="country-distances-graph"), lg=6, sm=12),
+            ]
+        ),
+    ]
+)
+
+# Callback for dropdown-b
+@app.callback(
+    Output("country-distances-dropdown-b", "options"),
+    Output("country-distances-dropdown-b", "value"),
+    Input("country-distances-dropdown-a", "value"),
+    Input("country-distances-dropdown-b", "options"),
+)
+def set_dropdown_b_options(dropdown_a_value, dropdown_b_options):
+    return (
+        [cntry for cntry in df[df.country != dropdown_a_value].country],
+        "Canada" if dropdown_b_options is None else no_update,
+    )
+
+
+# Callback for line_geo graph
+@app.callback(
+    Output("country-distances-graph", "figure"),
+    Output("country-distances-locations-graph", "figure"),
+    Input("country-distances-dropdown-a", "value"),
+    Input("country-distances-dropdown-b", "value"),
+)
+def make_line_geo_graph(country_a, country_b):
+
+    dff = df[df.country.isin([country_a, country_b])]
+
+    fig = px.line_geo(dff, locations="iso_alpha", projection="orthographic",)
+
+    fig_locations = px.line_geo(
+        dff, locations="iso_alpha", projection="orthographic", fitbounds="locations"
+    )
+
+    fig.update_traces(
+        line_width=3, line_color="red",
+    )
+
+    fig_locations.update_traces(
+        line_width=3, line_color="red",
+    )
+
+    return fig, fig_locations
+
+
+# Run the app
+if __name__ == "__main__":
+    app.run_server(debug=True)
