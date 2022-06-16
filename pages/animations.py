ec2125ecb10baaf1e3cc1f177f777eb6add06b64
Charming Data <info@charmingdata.org>
2022-06-16 12:02:01 -0400

    Update animations.py

--- pages/animations.py
+++ pages/animations.py
@@ -12,11 +12,14 @@ filename = __name__.split("pages.")[1]
 
 
 notes = """
+#### Dash Components in App:
+- [RadioItems](https://dash.plotly.com/dash-core-components/radioitems)
+- [Loading](https://dash.plotly.com/dash-core-components/loading)
 
 #### Plotly Documentation:  
-
 - [Intro to animations](https://plotly.com/python/animations/)
-
+- [Bar Plot](https://plotly.com/python/bar-charts/)
+- [Scatter Plot](https://plotly.com/python/line-and-scatter/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
