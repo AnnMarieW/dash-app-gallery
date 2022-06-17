33b4a04abdb5b98551f1f04f8b05fd49c44a1ce7
Somesh <42097653+someshfengde@users.noreply.github.com>
2022-06-17 17:09:04 +0000

    wrong links sorry

--- pages/plot-data-from-csv.py
+++ pages/plot-data-from-csv.py
@@ -10,8 +10,8 @@ filename = __name__.split("pages.")[1]
 
 notes = """
 #### Dash Components in App:
-- [Button](https://dash.plotly.com/dash-core-components/markdown)
-- [Graph](https://dash.plotly.com/dash-core-components/markdown)
+- [Button](https://dash.plotly.com/dash-html-components/button)
+- [Graph](https://dash.plotly.com/dash-core-components/graph)
 
 #### Plotly Documentation:  
 - [How plot data from csv file](https://plotly.com/python/plot-data-from-csv/)
