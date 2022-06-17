abf03f118fcb986e02eec123c1f05699827354e4
Somesh <42097653+someshfengde@users.noreply.github.com>
2022-06-17 17:02:51 +0000

    adding description and notes to plot-data-from-csv.py

--- pages/plot-data-from-csv.py
+++ pages/plot-data-from-csv.py
@@ -3,21 +3,21 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Plotting data from csv file")
+dash.register_page(__name__, description="This application plots stocks data collected from csv. It contains button to invert axis on graph.")
 
 filename = __name__.split("pages.")[1]
 
 
 notes = """
+#### Dash Components in App:
+- [Button](https://dash.plotly.com/dash-core-components/markdown)
+- [Graph](https://dash.plotly.com/dash-core-components/markdown)
 
 #### Plotly Documentation:  
-
 - [How plot data from csv file](https://plotly.com/python/plot-data-from-csv/)
 
-
-#### Contributed by:
+##### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
-
 """
 
 
