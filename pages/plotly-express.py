f5305a87fb46cef2a950ef7402024c5b7bb0763d
Charming Data <info@charmingdata.org>
2022-06-18 13:45:19 -0400

    Update plotly-express.py

--- pages/plotly-express.py
+++ pages/plotly-express.py
@@ -3,23 +3,21 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Plotly Express scatter matrix example. It has a top-bottom layout and a regular-callback. ")
+dash.register_page(__name__, description="Plotly Express scatter matrix example. It has a top-bottom layout and a regular-callback.")
 
 filename = __name__.split("pages.")[1]
 
 
 notes = """
+#### Dash Components in App:
+- [Dropdown](https://dash.plotly.com/dash-core-components/dropdown)
+
 #### Plotly Documentation:  
 - [Plotly Express Overview](https://plotly.com/python/plotly-express/)
 - [Scatter Matrix](https://plotly.com/python/splom/)
 
-#### Dash Components in App:
-- [Dropdown](https://dash.plotly.com/dash-core-components/dropdown)
-
-
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
-
 """
 
 
