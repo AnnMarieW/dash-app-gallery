10a76266906150707dbb924fc9dd6b496a561ec8
Somesh <42097653+someshfengde@users.noreply.github.com>
2022-06-18 22:37:28 +0530

    updated plotly-express.py

--- pages/plotly-express.py
+++ pages/plotly-express.py
@@ -3,19 +3,18 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Plotly Express scatter matrix example with top-down layout. This app contains adding components to graph via dropdown. ")
+dash.register_page(__name__, description="Plotly Express scatter matrix example with top-down layout. It has a top-bottom layout and a regular-callback. ")
 
 filename = __name__.split("pages.")[1]
 
 
 notes = """
-#### Dash Components in App:
-- [Markdown](https://dash.plotly.com/dash-core-components/markdown)
-- [Dropdown](https://plotly.com/python/dropdowns/)
-- [Scatter Matrix](https://plotly.com/python/splom/)
-
 #### Plotly Documentation:  
 - [Plotly Express Overview](https://plotly.com/python/plotly-express/)
+
+#### Dash Components in App:
+- [Dropdown](https://plotly.com/python/dropdowns/)
+- [Scatter Matrix](https://plotly.com/python/splom/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
