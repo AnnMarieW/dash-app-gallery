d804db75576ef9fa222368d8ae51931ca0b3c00a
emher <emil.h.eriksen@gmail.com>
2022-06-23 20:46:02 +0200

    Add title, change to dash2 imports, black formatting

--- examples/extend-data.py
+++ examples/extend-data.py
@@ -1,23 +1,24 @@
-import dash
-import dash_html_components as html
-import dash_core_components as dcc
 import numpy as np
+from dash import Dash, html, dcc, Input, Output, State
 
-from dash.dependencies import Input, Output, State
-
-# Example originates from SO post.
-https://stackoverflow.com/a/63681810/2428887
-# Example data (a circle).
+# Example from https://stackoverflow.com/a/63681810/2428887
 resolution = 1000
 t = np.linspace(0, np.pi * 2, resolution)
-x, y = np.cos(t), np.sin(t)
-# Example app.
-figure = dict(data=[{'x': [], 'y': []}], layout=dict(xaxis=dict(range=[-1, 1]), yaxis=dict(range=[-1, 1])))
-app = dash.Dash(__name__, update_title=None)  # remove "Updating..." from title
-app.layout = html.Div([
-    dcc.Graph(id='graph', figure=dict(figure)), dcc.Interval(id="interval", interval=25),
-    dcc.Store(id='offset', data=0), dcc.Store(id='store', data=dict(x=x, y=y, resolution=resolution)),
-])
+x, y = np.cos(t), np.sin(t)  # example data (a circle)
+figure = dict(
+    data=[{"x": [], "y": []}],
+    layout=dict(xaxis=dict(range=[-1, 1]), yaxis=dict(range=[-1, 1])),
+)
+app = Dash(__name__, update_title=None)  # remove "Updating..." from title
+app.layout = html.Div(
+    [
+        html.H4("Smooth updates using clientside callbacks"),
+        dcc.Graph(id="graph", figure=dict(figure)),
+        dcc.Interval(id="interval", interval=25),
+        dcc.Store(id="offset", data=0),
+        dcc.Store(id="store", data=dict(x=x, y=y, resolution=resolution)),
+    ]
+)
 app.clientside_callback(
     """
     function (n_intervals, data, offset) {
@@ -26,9 +27,10 @@ app.clientside_callback(
         return [[{x: [data.x.slice(offset, end)], y: [data.y.slice(offset, end)]}, [0], 500], end]
     }
     """,
-    [Output('graph', 'extendData'), Output('offset', 'data')],
-    [Input('interval', 'n_intervals')], [State('store', 'data'), State('offset', 'data')]
+    [Output("graph", "extendData"), Output("offset", "data")],
+    [Input("interval", "n_intervals")],
+    [State("store", "data"), State("offset", "data")],
 )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server()
