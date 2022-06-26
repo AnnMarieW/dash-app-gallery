bd32eae754b6336ddceeaeff1fd1f044ca40c89e
Tolgahan Cepel <tolgahan.cepel@gmail.com>
2022-06-26 23:40:39 +0300

    initial commit of the changes

--- pages/distplot.py
+++ pages/distplot.py
@@ -5,29 +5,20 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="Combined statistical representations. Adding box, violin or rug subplot to a histogram ",
+    description="Combined statistical representations. Adding box, violin or rug subplot to a histogram. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
 
-
 notes = """
+#### Dash Components in App:
+- [RadioItems](https://dash.plotly.com/dash-core-components/radioitems)
 
 #### Plotly Documentation:  
-
-- [Dist plots](https://plotly.com/python/distplot/)
-Combined statistical representations with px.histogram
-Several representations of statistical distributions are available in plotly, such as histograms,
- violin plots, box plots (see the complete list here). It is also possible to combine several representations in the same plot.
- 
- For example, the plotly.express function px.histogram can add a subplot with a different statistical representation 
- than the histogram, given by the parameter marginal.
-
+- [Distplots](https://plotly.com/python/distplot/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
-
 """
-
 
 layout = example_app(filename, notes=notes)
--- pages/figure-structure.py
+++ pages/figure-structure.py
@@ -3,21 +3,22 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Accessing Plotly figure structures in Dash")
+dash.register_page(
+    __name__,
+    description="Accessing Plotly figure structures in Dash. It has a top-bottom layout and a regular-callback."
+)
 
 filename = __name__.split("pages.")[1]
 
-
 notes = """
+#### Dash Components in App:
+- [Clipboard](https://dash.plotly.com/dash-core-components/clipboard)
 
-#### Plotly Documentation:  
-
+#### Plotly Documentation:
 - [The structure of a figure - data, traces and layout explained.](https://plotly.com/python/figure-structure/)
 
-
-#### Contributed by:
+##### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
-
 """
 
 layout = example_app(filename, notes=notes)
