2d1db07fc37f99dd4a835d5e2a183d8b9d9eee8d
Charming Data <info@charmingdata.org>
2022-06-29 14:10:38 -0400

    Update distplot.py

--- pages/distplot.py
+++ pages/distplot.py
@@ -5,7 +5,7 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="Combined statistical representations. Adding box, violin or rug subplot to a histogram. It has a top-bottom layout and a regular-callback.",
+    description="Combined statistical representations; adding box, violin or rug subplot to a histogram. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -16,6 +16,7 @@ notes = """
 
 #### Plotly Documentation:  
 - [Distplots](https://plotly.com/python/distplot/)
+- [Histograms](https://plotly.com/python/histograms/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
