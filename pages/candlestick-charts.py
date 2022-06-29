a8701abce3e7c2f626a6a8de779bdedf6c7de1f2
Charming Data <info@charmingdata.org>
2022-06-29 12:59:07 -0400

    Update pages/candlestick-charts.py

--- pages/candlestick-charts.py
+++ pages/candlestick-charts.py
@@ -13,7 +13,6 @@ notes = """
 - [Checklist](https://dash.plotly.com/dash-core-components/checklist)
 
 #### Plotly Documentation:  
-
 - [Candlestick charts](https://plotly.com/python/candlestick-charts/)
 - [RangeSlider](https://plotly.com/python/range-slider/)
 How to make interactive candlestick charts in Python with Plotly. Six examples of candlestick charts with Pandas, time series, and yahoo finance data.
