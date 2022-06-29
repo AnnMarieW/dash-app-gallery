62b5e989b9c7d6abe272ffb5de00af61fedf322f
Charming Data <info@charmingdata.org>
2022-06-29 00:50:00 -0400

    Update pages/planes_scatter_geo.py

--- pages/planes_scatter_geo.py
+++ pages/planes_scatter_geo.py
@@ -6,7 +6,7 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="This app shows connection between a dropdown and two graphs: scatter_geo (also called Bubble map) and scatter_mapbox. It has a top-bottom layout and a regular-callback.",
+    description="This app shows connection between a dropdown and two graphs: scatter_geo and scatter_mapbox. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
