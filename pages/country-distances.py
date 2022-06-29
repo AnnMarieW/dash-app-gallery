0fc50a78144624874a322796e28bff4fb296edaa
Charming Data <info@charmingdata.org>
2022-06-29 13:59:51 -0400

    Update country-distances.py

--- pages/country-distances.py
+++ pages/country-distances.py
@@ -5,15 +5,14 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="This application plots line on a map between the selected countries.",
+    description="This application plots line on a map between the selected countries. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
 
-# any notes will be displayed below the code-and-show page in a dcc.Markdown component
 notes = """
 #### Dash Components in App:
-- [Dropdown component](https://dash.plotly.com/dash-core-components/dropdown)
+- [Dropdown](https://dash.plotly.com/dash-core-components/dropdown)
 - [Chained Callback](https://dash.plotly.com/basic-callbacks#dash-app-with-chained-callbacks)
 
 #### Plotly Documentation:
