6a5ed9543053c31971dc580d665fe6ec975c18d4
Charming Data <info@charmingdata.org>
2022-06-29 13:53:02 -0400

    Update roc-and-pr-curves.py

--- pages/roc-and-pr-curves.py
+++ pages/roc-and-pr-curves.py
@@ -4,7 +4,7 @@ from utils.code_and_show import example_app
 
 
 dash.register_page(
-    __name__, description="This app does Analysis of the ML model's results using ROC and PR curves. This app uses top-down layout with regular callback."
+    __name__, description="This app does an Analysis of the ML model's results using ROC and PR curves. This app uses top-down layout with a regular callback."
 )
 
 filename = __name__.split("pages.")[1]
@@ -14,14 +14,10 @@ notes = """
 #### Dash Components in App:
 - [Dropdown](https://dash.plotly.com/dash-core-components/dropdown)
 
-
 #### Plotly Documentation:  
 - [ROC and PR Curves](https://plotly.com/python/roc-and-pr-curves/)
-Interpret the results of your classification using Receiver Operating Characteristics (ROC) and Precision-Recall (PR) Curves 
-- [area](https://plotly.com/python/filled-area-plots/)
-
-
-#### Community Components:
+- [Adding Lines to Figures](https://plotly.com/python/shapes/)
+- [Area](https://plotly.com/python/filled-area-plots/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
