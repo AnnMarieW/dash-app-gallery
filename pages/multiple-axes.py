ab694e0fd9e7fddeb7dc8046d4cea0f677924387
Milan Mitrovic <58112365+milanzmitrovic@users.noreply.github.com>
2022-06-29 06:54:48 +0200

    Milan finalize all files in pages (#58)
    * First 3 apps modified description.
    
    * Update pages/line-and-scatter.py
    
    * Update pages/knn-classification.py
    
    * update knn-classification.py file
    
    * update knn-classification.py file
    
    * modify legend.py
    
    * modify line and scatter file
    
    * Updated rest of apps
    
    * erase line break pages/legend.py
    
    * Update pages/line-charts.py
    
    * Update pages/marker-style.py
    
    * Update pages/marker-style.py
    
    * Update pages/multiple-axes.py
    
    * Update pages/network-graphs.py
    
    * Update pages/network-graphs.py
    
    * Update pages/parallel-coord.py
    
    * Update pages/planes_scatter_geo.py
    
    * Update pages/planes_scatter_geo.py
    
    Co-authored-by: Charming Data <info@charmingdata.org>

--- pages/multiple-axes.py
+++ pages/multiple-axes.py
@@ -4,22 +4,23 @@ from utils.code_and_show import example_app
 
 
 dash.register_page(
-    __name__, description="Interactive data-scaling using the secondary axis"
+    __name__, description="This app shows interactive data-scaling using the secondary axis. It has a top-bottom layout and a regular-callback."
 )
 
 filename = __name__.split("pages.")[1]
 
 
 notes = """
+#### Dash Components in App:
+- [Radio Items](https://dash.plotly.com/dash-core-components/radioitems)
 
 #### Plotly Documentation:  
-
 - [Graph with multiple axes (dual y-axis plots, plots with secondary axes)](https://plotly.com/python/multiple-axes/)
-
+- [Scatter Plot](https://plotly.com/python/line-and-scatter/)
+- [Plotly Subplots](https://plotly.com/python/subplots/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
-
 """
 
 
