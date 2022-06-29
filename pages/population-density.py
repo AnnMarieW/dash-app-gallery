30caff1c190b8760dc07b0c722fc700351b62e80
Charming Data <info@charmingdata.org>
2022-06-29 13:45:05 -0400

    Update population-density.py

--- pages/population-density.py
+++ pages/population-density.py
@@ -3,6 +3,7 @@ import dash
 from utils.code_and_show import example_app
 
 
+
 dash.register_page(
     __name__,
     description="This app shows Population Density Map by Year. It uses a top-down layout.",
@@ -11,18 +12,11 @@ dash.register_page(
 filename = __name__.split("pages.")[1]
 
 notes = """
-#### Dash Components in App:
-
 #### Plotly Documentation:   
 - [choropleth](https://plotly.com/python/choropleth-maps/)
 
-#### Community Components:
-
-Dash Bootstrap Components 
-- [Row and container](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/layout/)
-
 ##### Contributed by:
-This example app was contributed by [Plotly](https://plotly.com/python/)
+This example app was contributed by [Tolga](https://github.com/tolgahancepel)
 """
 
-layout = example_app(filename)
+layout = example_app(filename, notes=notes)
