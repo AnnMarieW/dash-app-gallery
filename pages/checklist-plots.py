712f48f2236d5a6966f7aeff14f4ce84789b877a
Charming Data <info@charmingdata.org>
2022-06-29 13:05:41 -0400

    Update pages/checklist-plots.py

--- pages/checklist-plots.py
+++ pages/checklist-plots.py
@@ -11,7 +11,7 @@ filename = __name__.split("pages.")[1]
 
 notes = """
 #### Dash Components in App:
-- [Dcc.Checklist](https://dash.plotly.com/dash-core-components/checklist)
+- [Checklist](https://dash.plotly.com/dash-core-components/checklist)
 
 
 #### Plotly Documentation:  
