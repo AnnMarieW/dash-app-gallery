da510c284adbd4c1680219ce162e25d55d537966
Pierre-Olivier Bonin, Ph.D <pierre-olivier.bonin@hotmail.com>
2022-07-02 13:37:41 -0400

    Changing the ecdf app back to its version in the previous PR.

--- examples/ecdf-app.py
+++ examples/ecdf-app.py
@@ -5,40 +5,50 @@ import plotly.express as px
 filepath = "https://raw.githubusercontent.com/plotly/datasets/master/mtl2013ternary.csv"
 df = pd.read_csv(filepath)
 
-df["Coderre_pct"] = round(df["Coderre"]/df["total"]*100, 2)
-df["Joly_pct"] = round(df["Joly"]/df["total"]*100, 2)
-df["Bergeron_pct"] = round(df["Bergeron"]/df["total"]*100, 2)
+df["Coderre_pct"] = round(df["Coderre"] / df["total"] * 100, 2)
+df["Joly_pct"] = round(df["Joly"] / df["total"] * 100, 2)
+df["Bergeron_pct"] = round(df["Bergeron"] / df["total"] * 100, 2)
 
 candidate_list = ["Coderre_pct", "Joly_pct", "Bergeron_pct"]
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H1("ECDF of Vote Share by District in Montreal's Municipal Elections, 2013",
-            style={'textAlign': 'center'}),
-    html.Div(
-        [html.H3('Choose a candidate:',
-                 style={"paddingLight": "60px"}),
-         dcc.Dropdown(
-            id='ecdf-app-x-candidate-dropdown',
-            value='Joly_pct',
-            options=candidate_list,
-            multi=True
-        )], style={"display": "inline-block", "verticalAlign": "top", "width": "30%", "margin-left": "5em"}),
-    dcc.Graph(
-        id='ecdf-app-x-graph'
-    )
-])
+app.layout = html.Div(
+    [
+        html.H1(
+            "ECDF of Vote Share by District in Montreal's Municipal Elections, 2013",
+            style={"textAlign": "center"},
+        ),
+        html.Div(
+            [
+                html.H3("Choose a candidate:", style={"paddingLight": "60px"}),
+                dcc.Dropdown(
+                    id="ecdf-app-x-candidate-dropdown",
+                    value="Joly_pct",
+                    options=candidate_list,
+                    multi=True,
+                ),
+            ],
+            style={
+                "display": "inline-block",
+                "verticalAlign": "top",
+                "width": "30%",
+                "margin-left": "5em",
+            },
+        ),
+        dcc.Graph(id="ecdf-app-x-graph"),
+    ]
+)
 
 
-@ app.callback(
-    Output('ecdf-app-x-graph', 'figure'),
-    [Input("ecdf-app-x-candidate-dropdown", "value")])
+@app.callback(
+    Output("ecdf-app-x-graph", "figure"),
+    Input("ecdf-app-x-candidate-dropdown", "value"),
+)
 def update_graph(candidate):
     fig = px.ecdf(df, x=candidate)
-    fig.update_layout()
     return fig
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run_server(debug=True)
