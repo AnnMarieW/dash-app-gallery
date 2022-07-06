4de97c7346058edda1c551af868f7f5ef300cf3d
Charming Data <info@charmingdata.org>
2022-07-06 16:26:02 -0400

    Update time-series.py

--- pages/time-series.py
+++ pages/time-series.py
@@ -4,7 +4,7 @@ from utils.code_and_show import example_app
 
 
 dash.register_page(
-    __name__, description="Stock price analysis - plotting time series data"
+    __name__, description="This app uses a dropdown to display stock prices on a line chart by Stock name. It uses a top-bottom layout and a regular-callback."
 )
 
 filename = __name__.split("pages.")[1]
@@ -12,12 +12,14 @@ filename = __name__.split("pages.")[1]
 
 notes = """
 
-#### Plotly Documentation:  
+#### Dash Components in the App:
+- [Dropdown](https://dash.plotly.com/dash-core-components/dropdown)
 
+#### Plotly documentation:  
 - [How plot time series data](https://plotly.com/python/time-series/)
+- [Line Chart](https://plotly.com/python/line-charts/)
 
-
-#### Contributed by:
+##### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
 
 """
