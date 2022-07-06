f27d4e8d40d491210a05a2bc88bd6e7da562350d
Charming Data <info@charmingdata.org>
2022-07-06 11:22:12 -0400

    Update pages/axes.py

--- pages/axes.py
+++ pages/axes.py
@@ -20,7 +20,6 @@ notes = """
 - [Adjusting the axes properties](https://plotly.com/python/axes/)
 - [Histogram](https://plotly.com/python/histograms)
 
-Learn how to adjust axes properties in Python - axes titles, styling and coloring axes and grid lines, ticks, tick labels and more.
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
