624e6b19fea09724a2abc61c3fa75a2fb7a63bde
Charming Data <info@charmingdata.org>
2022-07-06 15:58:03 -0400

    Update sunburst-plot.py

--- pages/sunburst-plot.py
+++ pages/sunburst-plot.py
@@ -5,7 +5,7 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="Demonstrate use of Sunburst chart with continuous color adjustable via dropdown. This app uses top-down layout with regular callback.",
+    description="Demonstrate use of Sunburst chart with continuous color adjustable via dropdown. This app uses top-down layout with regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -17,10 +17,6 @@ notes = """
 
 #### Plotly Documentation:  
 - [Sunburst plots](https://plotly.com/python/sunburst-charts/)
-
-#### Community Components:
-Dash Bootstrap Components 
-- [Col,Container and Row](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/layout/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
