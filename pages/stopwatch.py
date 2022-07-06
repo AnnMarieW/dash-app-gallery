94aea4f0fdf425909df7a6853b0918579acaa739
Charming Data <info@charmingdata.org>
2022-07-06 15:52:44 -0400

    Update stopwatch.py

--- pages/stopwatch.py
+++ pages/stopwatch.py
@@ -5,7 +5,7 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="Stopwatch timer application with 1 button and an input field.This app uses top-down layout with regular callback.",
+    description="Stopwatch timer application with 1 button and an input field. This app uses top-down layout with regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -13,9 +13,9 @@ filename = __name__.split("pages.")[1]
 
 notes = """
 #### Dash Components in App:
-- [Input component](https://dash.plotly.com/dash-core-components/input)
-- [Input component](https://dash.plotly.com/dash-core-components/interval)
-- [Dash Daq components](https://dash.plotly.com/dash-daq)
+- [Input](https://dash.plotly.com/dash-core-components/input)
+- [Interval](https://dash.plotly.com/dash-core-components/interval)
+- [Power Button](https://dash.plotly.com/dash-daq/powerbutton)
 - [LED Display](https://dash.plotly.com/dash-daq/leddisplay)
 
 ##### Contributed by:
