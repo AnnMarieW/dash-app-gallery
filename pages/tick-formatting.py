832e21d150ea630e07a36c80da7ac2a003b6e72b
Charming Data <info@charmingdata.org>
2022-07-06 16:22:01 -0400

    Update tick-formatting.py

--- pages/tick-formatting.py
+++ pages/tick-formatting.py
@@ -3,19 +3,21 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Linear Tick Formatting")
+dash.register_page(__name__, description="This app uses the checklist to allow for linear ticks in the scatter plot. It has a top-bottom layout and a regular-callback.")
 
 filename = __name__.split("pages.")[1]
 
 
 notes = """
 
-#### Plotly Documentation:  
+#### Dash Components in the App:
+- [Checklist](https://dash.plotly.com/dash-core-components/checklist)
 
+#### Plotly documentation:  
 - [How to format axes ticks ](https://plotly.com/python/tick-formatting/)
+- [Scatter Plot](https://plotly.com/python/line-and-scatter/)
 
-
-#### Contributed by:
+##### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
 
 """
