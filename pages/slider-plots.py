4880dbdf8a2e5fa15554bf9540a75f4ad9554f95
Charming Data <info@charmingdata.org>
2022-07-06 13:22:35 -0400

    Update slider-plots.py

--- pages/slider-plots.py
+++ pages/slider-plots.py
@@ -5,7 +5,7 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="This app plots the iris data on a bar chart filtered by a slider. This app uses top-down layout with regular callback.",
+    description="This app plots the iris data on a bar chart filtered by a slider. This app uses top-down layout with regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -15,10 +15,11 @@ notes = """
 - [Slider](https://dash.plotly.com/dash-core-components/slider)
 
 #### Plotly Documentation:  
-- [bar](https://plotly.com/python/bar-charts/)
+- [Bar Plot](https://plotly.com/python/bar-charts/)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
+
 """
 
 
