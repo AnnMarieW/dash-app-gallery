b7c037173b972492105e9bd3c8e3c9289562890b
Charming Data <info@charmingdata.org>
2022-07-06 16:00:28 -0400

    Update table.py

--- pages/table.py
+++ pages/table.py
@@ -3,13 +3,13 @@ import dash
 from utils.code_and_show import example_app
 
 
-dash.register_page(__name__, description="Use of DataTable with callback on cell click. This app uses top-down layout with regular callback.")
+dash.register_page(__name__, description="Use of DataTable with callback on cell click. This app uses top-down layout with regular-callback.")
 
 filename = __name__.split("pages.")[1]
 
 notes = """
-#### Dash components in App:  
-- [DataTable component](https://dash.plotly.com/datatable)
+#### Dash Components in App:  
+- [DataTable](https://dash.plotly.com/datatable)
 
 #### Contributed by:
 This example app was contributed by [Plotly](https://plotly.com/python/)
