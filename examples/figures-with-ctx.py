1d5643ec99c8a5d60e9742edb660010dd709f147
Tolgahan Cepel <tolgahan.cepel@gmail.com>
2022-07-10 12:11:11 +0300

    removed the png and used dbc button

--- examples/figures-with-ctx.py
+++ examples/figures-with-ctx.py
@@ -3,24 +3,19 @@ from dash import Input, Output, dcc, html, ctx
 import plotly.express as px
 import dash_bootstrap_components as dbc
 
-# Reading the data
 data_canada = px.data.gapminder().query("country == 'Canada'")
 
-# Defining the Dash application
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
-server = app.server
 
 buttons = html.Div(
     [
-        html.Button("Bar chart", id="figures-with-ctx-x-btn-bar"),
-        html.Br(),
-        html.Button("Line chart", id="figures-with-ctx-x-btn-line"),
-        html.Br(),
-        html.Button("Filled area chart", id="figures-with-ctx-x-btn-area"),
-    ]
+        dbc.Button("Bar chart", id="figures-with-ctx-x-btn-bar", color="secondary"),
+        dbc.Button("Line chart", id="figures-with-ctx-x-btn-line", color="secondary"),
+        dbc.Button("Filled area chart", id="figures-with-ctx-x-btn-area", color="secondary"),
+    ],
+    className="d-grid gap-2",
 )
 
-# Application layout
 app.layout = dbc.Container(
     [
         html.H1("Changing figures with callback_context (CTX)"),
@@ -35,7 +30,6 @@ app.layout = dbc.Container(
     ]
 )
 
-# Callback
 @app.callback(
     Output("figures-with-ctx-x-graph", "figure"),
     Input("figures-with-ctx-x-btn-bar", "n_clicks"),
@@ -53,6 +47,5 @@ def display(btn_bar, btn_line, btn_area):
         return px.area(data_canada, x="year", y="pop")
 
 
-# Run the app
 if __name__ == "__main__":
     app.run_server(debug=True)
