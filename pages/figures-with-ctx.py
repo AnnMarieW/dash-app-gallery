c109d09ef690917b157c3217e80c542bb6e1f250
Charming Data <info@charmingdata.org>
2022-07-11 13:29:04 -0400

    updated link from dash button docs to dbc button docs. (#82)
    * "added text to upper right box"
    
    * "getting started first app"
    
    * Update link to dash bootstrap button docs from the html button docs

--- pages/figures-with-ctx.py
+++ pages/figures-with-ctx.py
@@ -12,13 +12,17 @@ filename = __name__.split("pages.")[1]
 
 notes = """
 #### Dash Components in App:
-- [Button](https://dash.plotly.com/dash-html-components/button)
 - [Callback Context](https://dash.plotly.com/determining-which-callback-input-changed)
 
 #### Plotly Documentation:
 - [Bar charts]((https://plotly.com/python/bar-charts/))
 - [Line charts](https://plotly.com/python/line-charts/)
 - [Filled Area Plots](https://plotly.com/python/filled-area-plots/)
+
+#### Community Components:
+
+Dash Bootstrap Components 
+- [Button](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/button/ "This component was made by the community and not officially maintained by Plotly.")
 
 ##### Contributed by:
 This example app was contributed by [tolgahancepel](https://github.com/tolgahancepel)
