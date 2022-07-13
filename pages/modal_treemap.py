f66aa78b79a00f63fb4d4b8be2deac90fbfeb403
Charming Data <info@charmingdata.org>
2022-07-13 14:13:45 -0400

    Update modal_treemap.py

--- pages/modal_treemap.py
+++ pages/modal_treemap.py
@@ -1,11 +1,10 @@
-
 import dash
 
-from utils.code_and_show import example_app
+from lib.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="This application shows Treemap chart with modal and dropdown filter. It has a top-bottom layout and a regular-callback.",
+    description="This application shows treemap and sunburst charts with a modal, dropdown and datepicker. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -24,7 +23,7 @@ notes = """
 
 Dash Bootstrap Components 
 - [Modal](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/modal/ "This component was made by the community and not officially maintained by Plotly.")
-- [Navbar](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/navbar/)
+- [Button](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/button/ "This component was made by the community and not officially maintained by Plotly.")
 
 ##### Contributed by:
 This example app was contributed by [Milan Mitrovic](https://milanzmitrovic.github.io)
