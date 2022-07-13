bdb8835d2a3f51af7a0c3bf43a15c1e5087f51f0
Adam Schroeder <adamschroeder.m@gmail.com>
2022-07-13 15:09:04 -0400

    updated text from 150 to ~100 lines of code

--- lib/overview_textbox.py
+++ lib/overview_textbox.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 content = """
 ### Welcome to the Dash Example Index 
 
-This is a community-supported project designed for people new to Plotly and Dash. It contains minimal sample apps with ~150 lines of code to demonstrate basic usage of graphs, components, callbacks, and layout design. 
+This is a community-supported project designed for people new to Plotly and Dash. It contains minimal sample apps with ~100 lines of code to demonstrate basic usage of graphs, components, callbacks, and layout design. 
 
 If you have any suggestions for improvements, please open a [new issue](https://github.com/AnnMarieW/dash-app-gallery/issues).
 If you would like to contribute your own app, checkout the [contributing guide](https://github.com/AnnMarieW/dash-app-gallery/blob/main/CONTRIBUTING.md). 
