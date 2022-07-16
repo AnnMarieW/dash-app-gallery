bc22df03f8ac1cb6e75b66be05692ea7c51c75a4
adam schroeder <adamschroeder.m@gmail.com>
2022-07-16 01:51:06 -0400

    modify icicle app

--- pages/offCanvas_icicle.py
+++ pages/offCanvas_icicle.py
@@ -1,12 +1,12 @@
 
 import dash
 
-from utils.code_and_show import example_app
+from lib.code_and_show import example_app
 
 
 dash.register_page(
     __name__,
-    description="This application shows Icicle chart, modal and dropdown. It has a top-bottom layout and a regular-callback.",
+    description="This application shows an Icicle chart, modal and dropdown. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -22,8 +22,8 @@ notes = """
 #### Community Components:
 
 Dash Bootstrap Components 
-- [Modal](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/modal/ "This component was made by the community and not officially maintained by Plotly.")
-- [Navbar](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/navbar/ "This component was made by the community and not officially maintained by Plotly.")
+- [Button](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/button/ "This component was made by the community and not officially maintained by Plotly.")
+- [Offcanvas](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/offcanvas/ "This component was made by the community and not officially maintained by Plotly.")
 
 ##### Contributed by:
 This example app was contributed by [Milan Mitrovic](https://milanzmitrovic.github.io)
