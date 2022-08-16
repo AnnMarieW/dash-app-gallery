53c4e422ffd6dc7cbafdbc04df384ed9c3602467
Adam <adam@charming-data.com>
2022-08-16 16:42:26 -0400

    Update pages/download-plots.py

--- pages/download-plots.py
+++ pages/download-plots.py
@@ -5,7 +5,7 @@ from utils.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="Application for downloading the graph plots. This app has a top-bottom layout and a regular-callback."
+    description="Application for text file reports. This app has a top-bottom layout and a ctx callback."
 )
 
 filename = __name__.split("pages.")[1]
