5d3c08d1df599f354a6deae861c26bdc7e0c6cb5
Somesh <42097653+someshfengde@users.noreply.github.com>
2022-08-19 20:23:14 +0530

    Submitting download-plots.py app  (#102)
    * adding data scientists salaries choropleth app
    
    * adding download_plots app
    
    * removing data scientist salaries.py
    
    * removing page file
    
    * Delete sample_fig.svg
    
    * Delete sample_fig.svg
    
    * Update examples/download-plots.py
    
    * Update examples/download-plots.py
    
    * Update pages/download-plots.py
    
    * Rename download-plots.py to download-report.py
    
    * Rename download-plots.py to download-report.py
    
    * Update pages/download-report.py
    
    * Update examples/download-report.py
    
    * Update examples/download-report.py
    
    * Update pages/download-report.py
    
    * Update pages/download-report.py
    
    * Update examples/download-report.py
    
    * Update examples/download-report.py
    
    Co-authored-by: Adam <adam@charming-data.com>

---
+++ examples/download-report.py
@@ -0,0 +1,67 @@
+import random
+from dash import Dash, dcc, html, Input, Output, State, callback
+import plotly.express as px
+
+
+app = Dash(__name__)
+
+initial_x = [random.randint(1, 50) for _ in range(15)]
+initial_y = [random.randint(1, 50) for _ in range(15)]
+
+
+def report(x, y, n):
+    return (
+        f"""
+        Report #{n}  
+         - The max X point is {max(x)}
+         - The max Y point is {max(y)}
+         - The sum of X points are {sum(x)}
+         - The sum of Y points are {sum(y)}
+        """)
+
+
+app.layout = html.Div(
+    [
+        html.H3("Build a Graph and Download Report", style={"textAlign": "center"}),
+        html.Button("Generate values", id="download-report-x-random-val", n_clicks=0),
+        html.Button("Download report", id="download-report-x-save-button"),
+        html.Div("Remember that you can use the camera icon right above the graph to download an image of the figure",
+                 style={'textAlign':'right'}),
+        dcc.Graph(
+            id="download-report-x-graph",
+            figure=px.scatter(x=initial_x, y=initial_y, title="Figure #0"),
+        ),
+        dcc.Markdown(
+            report(initial_x, initial_y, 0),
+            id="download-report-x-onscreen-report"),
+        dcc.Download(id="download-report-x-download-component"),
+    ]
+)
+
+
+@callback(
+    Output("download-report-x-graph", "figure"),
+    Output("download-report-x-onscreen-report", "children"),
+    Input("download-report-x-random-val", "n_clicks"),
+    prevent_initial_call=True,
+)
+def update_report(n):
+    x = [random.randint(1, 50) for _ in range(15)]
+    y = [random.randint(1, 50) for _ in range(15)]
+    fig = px.scatter(x=x, y=y, title=f"Figure #{n}")
+    return fig, report(x, y, n)
+
+
+@callback(
+    Output("download-report-x-download-component", "data"),
+    Input("download-report-x-save-button", "n_clicks"),
+    State("download-report-x-onscreen-report", "children"),
+    prevent_initial_call=True,
+)
+def download_report(_, current_report):
+    return dict(content=current_report, filename="my-report.txt")
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
+
---
+++ pages/download-report.py
@@ -0,0 +1,29 @@
+import dash
+
+from utils.code_and_show import example_app
+
+
+dash.register_page(
+    __name__,
+    description="Application for text file reports. This app has a top-bottom layout and a regular callback."
+)
+
+filename = __name__.split("pages.")[1]
+
+
+notes = """
+#### Dash Components in App:
+- [Button](https://dash.plotly.com/dash-html-components/button)
+- [Markdown](https://dash.plotly.com/dash-core-components/markdown)
+- [Download](https://dash.plotly.com/dash-core-components/download)
+
+#### Plotly Documentation:  
+- [Scatter Plot](https://plotly.com/python/line-and-scatter/)
+
+#### Contributed by:
+This example app was contributed by [Someshfengde](https://github.com/someshfengde)
+
+"""
+
+
+layout = example_app(filename, notes=notes)
