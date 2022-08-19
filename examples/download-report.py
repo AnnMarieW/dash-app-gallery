61dda158eb09f75f7951ab3ac0fda55ca32d7452
Adam <adam@charming-data.com>
2022-08-19 10:27:03 -0400

    Update examples/download-report.py

--- examples/download-report.py
+++ examples/download-report.py
@@ -1,27 +1,37 @@
-import dash
+import random
 from dash import Dash, dcc, html, Input, Output, State, ctx, callback
 import plotly.express as px
-import random
+
 
 app = Dash(__name__)
+
 initial_x = [random.randint(1, 50) for _ in range(15)]
 initial_y = [random.randint(1, 50) for _ in range(15)]
-def report(x,y):
-    return f"The max X point is {max(x)} \nThe max Y point is {max(y)} \nThe sum of X points are {sum(x)} \nThe sum of Y points are {sum(y)}"
+
+
+def report(x, y, n):
+    return (
+        f"""
+        Report #{n}  
+         - The max X point is {max(x)}
+         - The max Y point is {max(y)}
+         - The sum of X points are {sum(x)}
+         - The sum of Y points are {sum(y)}
+        """)
+
 
 app.layout = html.Div(
     [
         html.H3("Build a Graph and Download Report", style={"textAlign": "center"}),
+        html.Button("Generate values", id="download-report-x-random-val", n_clicks=0),
         html.Button("Download report", id="download-report-x-save-button"),
         dcc.Graph(
-            id="download-report-x-graph", figure=px.scatter(x=initial_x, y=initial_y)
+            id="download-report-x-graph",
+            figure=px.scatter(x=initial_x, y=initial_y, title="Figure #0"),
         ),
-        html.Button("Generate values", id="download-report-x-random-val", n_clicks=0),
         dcc.Markdown(
-            id="download-report-x-onscreen-report",
-            style={"white-space": "pre"},
-            children=report(initial_x, initial_y),
-        ),
+            report(initial_x, initial_y, 0),
+            id="download-report-x-onscreen-report"),
         dcc.Download(id="download-report-x-download-component"),
     ]
 )
@@ -30,22 +40,24 @@ app.layout = html.Div(
 @callback(
     Output("download-report-x-graph", "figure"),
     Output("download-report-x-onscreen-report", "children"),
-    Output("download-report-x-download-component", "data"),
     Input("download-report-x-random-val", "n_clicks"),
+    prevent_initial_call=True,
+)
+def update_report(n):
+    x = [random.randint(1, 50) for _ in range(15)]
+    y = [random.randint(1, 50) for _ in range(15)]
+    fig = px.scatter(x=x, y=y, title=f"Figure #{n}")
+    return fig, report(x, y, n)
+
+
+@callback(
+    Output("download-report-x-download-component", "data"),
     Input("download-report-x-save-button", "n_clicks"),
     State("download-report-x-onscreen-report", "children"),
     prevent_initial_call=True,
 )
-def display_choropleth(get_random_values, download_clicks, current_report):
-    if ctx.triggered_id == "download-report-x-random-val":
-        x = [random.randint(1, 50) for _ in range(15)]
-        y = [random.randint(1, 50) for _ in range(15)]
-        fig = px.scatter(x=x, y=y)
-        return fig, report(x,y), dash.no_update
-
-    if ctx.triggered_id == "download-report-x-save-button":
-        save_report = dict(content=current_report, filename="my-report.txt")
-        return dash.no_update, dash.no_update, save_report
+def download_report(_, current_report):
+    return dict(content=current_report, filename="my-report.txt")
 
 
 if __name__ == "__main__":
