8bdfa2c09581b16213dbd3ed6e3a9d7e82f0902d
Adam <adamschroeder.m@gmail.com>
2022-08-31 17:09:47 -0400

    Update examples/cheatsheets/share.py

--- examples/cheatsheets/share.py
+++ examples/cheatsheets/share.py
@@ -1,21 +1,5 @@
 deploy = """
 
-#### Dash Tools
-
-`pip install dash-tools`  
-
-See `dash-tools` getting started [documentation](https://dash-tools.readthedocs.io/en/latest/getting%20started.html)
-
-```
-dashtools init MyApp
-dashtools heroku --deploy
-dashtools heroku --update
-
-
-
-```
-
---------------
 #### Dash Enterprise Deploy
 Requirements.txt:
 ```
