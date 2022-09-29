4b926dbb1bcaa48f832d16ebb7127b11f76e228e
Pierre-Olivier Bonin, Ph.D <pierre-olivier.bonin@hotmail.com>
2022-09-29 11:34:18 -0400

    Adding new and improved version of circular callback app (#111)
    * adding circular callback app
    
    * committing new improved version of circular callback app

---
+++ examples/circular-callback-app.py
@@ -0,0 +1,61 @@
+from dash import Dash, dcc, html, Input, Output, ctx
+import pandas as pd
+import plotly.express as px
+
+filepath = "https://raw.githubusercontent.com/plotly/datasets/master/volcano_db.csv"
+df = pd.read_csv(filepath, encoding="latin")
+
+external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.layout = html.Div([
+    html.Header("Geographic Distribution of Volcanoes by Height",
+                style={"font-size": "30px"}),
+    html.Div('Minimum Volcano Height',
+             style={"font-size": "20px"}),
+    'Meter ',
+    dcc.Input(
+        id="circular-callback-x-meter",
+        value=2000,
+        type="number"
+    ),
+    ' to Feet ',
+    dcc.Input(
+        id="circular-callback-x-feet",
+        value=6561.68,
+        type="number",
+    ),
+    dcc.Graph(
+        id="circular-callback-x-map"
+    )
+],
+    style={"margin": 30})
+
+
+@ app.callback(
+    Output("circular-callback-x-meter", "value"),
+    Output("circular-callback-x-feet", "value"),
+    Output('circular-callback-x-map', 'figure'),
+    Input("circular-callback-x-meter", "value"),
+    Input("circular-callback-x-feet", "value"),
+)
+def sync_input(meter, feet):
+    if ctx.triggered_id == "circular-callback-x-meter":
+        feet = None if meter is None else round((float(meter) * 3.28084), 1)
+    else:
+        meter = None if feet is None else round((float(feet)/3.28084), 1)
+
+    fig = px.scatter_geo(data_frame=df.loc[df["Elev"] >= meter],
+                         lat="Latitude",
+                         lon="Longitude",
+                         size="Elev",
+                         hover_name="Volcano Name",
+                         projection="natural earth",
+                         width=1300,
+                         height=900)
+    fig.update_layout()
+    return meter, feet, fig
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
