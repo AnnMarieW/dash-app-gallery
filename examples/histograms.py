f56484bbe3b62dc344999c7755e7ffa4505f19e9
Ann Marie Ward <72614349+AnnMarieW@users.noreply.github.com>
2022-09-30 06:40:32 -0700

    updated histogram app (#112)
    * updated histogram app
    
    * updated description

--- examples/histograms.py
+++ examples/histograms.py
@@ -1,35 +1,80 @@
-from dash import Dash, dcc, html, Input, Output
-import plotly.express as px
+from dash import Dash, html, dcc, Input, Output
 import numpy as np
+import plotly.express as px
+import dash_bootstrap_components as dbc
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB])
 
+heading = html.H4(
+    "Normal Distribution Simulation", className="bg-primary text-white p-2"
+)
+
+
+sample_size = html.Div(
+    [
+        dbc.Label("Sample Size", html_for="size"),
+        dcc.Slider(1, 1000, value=250, id="histograms-x-size",
+            tooltip={"placement": "bottom", "always_visible": True},
+        ),
+    ],
+    className="mt-2",
+)
+
+
+n_bins = html.Div(
+    [
+        dbc.Label("Number of Bins", html_for="n_bins"),
+        dcc.Slider(1, 100, 10, value=20, id="histograms-x-n_bins",
+            tooltip={"placement": "bottom", "always_visible": True},
+        ),
+    ],
+    className="mt-2",
+)
+
+mean = html.Div(
+    [dbc.Label("Mean", html_for="mean"), dbc.Input(id="histograms-x-mean", type="number", value=0)],
+    className="mt-2",
+)
+
+std_dev = html.Div(
+    [
+        dbc.Label("Standard Deviation", html_for="std_dev"),
+        dbc.Input(id="histograms-x-std_dev",type="number", value=1),
+    ],
+    className="mt-2",
+)
+
+control_panel = dbc.Card(
+    dbc.CardBody(
+        [sample_size, n_bins, mean, std_dev],
+        className="bg-light",
+    )
+)
+
+graph = dbc.Card(
+    [html.Div(id="histograms-x-error_msg", className="text-danger"), dcc.Graph(id="histograms-x-graph")]
+)
 
 app.layout = html.Div(
-    [
-        html.H4("Interactive normal distribution"),
-        dcc.Graph(id="histograms-x-graph"),
-        html.P("Mean:"),
-        dcc.Slider(
-            id="histograms-x-mean", min=-3, max=3, value=0, marks={-3: "-3", 3: "3"}
-        ),
-        html.P("Standard Deviation:"),
-        dcc.Slider(
-            id="histograms-x-std", min=1, max=3, value=1, marks={1: "1", 3: "3"}
-        ),
-    ]
+    [heading, dbc.Row([dbc.Col(control_panel, md=4), dbc.Col(graph, md=8)])]
 )
 
 
 @app.callback(
     Output("histograms-x-graph", "figure"),
+    Output("histograms-x-error_msg", "children"),
     Input("histograms-x-mean", "value"),
-    Input("histograms-x-std", "value"),
+    Input("histograms-x-std_dev", "value"),
+    Input("histograms-x-n_bins", "value"),
+    Input("histograms-x-size", "value"),
 )
-def display_color(mean, std):
-    data = np.random.normal(mean, std, size=500)
-    fig = px.histogram(data, range_x=[-10, 10])
-    return fig
+def callback(m, std_dev, n_bins, n):
+    if m is None or std_dev is None:
+        return {}, "Please enter Standard Deviation and Mean"
+    if std_dev < 0:
+        return {}, "Please enter Standard Deviation > 0"
+    data = np.random.normal(m, std_dev, n)
+    return px.histogram(data, nbins=n_bins), None
 
 
 if __name__ == "__main__":
--- pages/histograms.py
+++ pages/histograms.py
@@ -1,11 +1,11 @@
 import dash
 
-from lib.code_and_show import example_app
+from lib.code_and_show import example_app, make_app_first
 
 
 dash.register_page(
     __name__,
-    description="Interactive histogram where the Slider sets the mean and standard deviation. It has a top-bottom layout and a regular-callback.",
+    description="Interactive histogram where the mean, standard deviation sample size and number of bins is set with sliders and numeric inputs. It has a control panel in a sidebar in a responsive side-by-side layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -21,4 +21,4 @@ notes = """
 This example app was contributed by [Plotly](https://plotly.com/python/)
 """
 
-layout = example_app(filename, notes=notes)
+layout = example_app(filename, notes=notes, make_layout=make_app_first)
