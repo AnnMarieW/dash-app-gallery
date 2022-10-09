7c6ff9be5fb4f42be9522900cc63b555450d0db2
AnnMarieW <amward@fastmail.com>
2022-10-09 10:51:13 -0700

    add image and black

--- examples/tooltip.py
+++ examples/tooltip.py
@@ -1,4 +1,3 @@
-
 from dash import Dash, dcc, html, Input, Output, no_update
 import pandas as pd
 import plotly.express as px
@@ -15,25 +14,21 @@ df["Profit"] = df["Profit"].astype("float")
 
 
 # Transforming 'Order Date' column into yyyy-mm format
-df['year_month'] = pd.DatetimeIndex(df['Order Date']).to_period('M').astype(str)
+df["year_month"] = pd.DatetimeIndex(df["Order Date"]).to_period("M").astype(str)
 
 # Grouping data as per 'Order Date' dimension.
 # Getting time series - profit per month.
-df_grouped = df[['Order Date', 'Profit']].groupby(
-    by=pd.Grouper(
-        key='Order Date',
-        axis=0, freq='M'
-    )).sum().reset_index()
+df_grouped = (
+    df[["Order Date", "Profit"]]
+    .groupby(by=pd.Grouper(key="Order Date", axis=0, freq="M"))
+    .sum()
+    .reset_index()
+)
 
 # Creating base bar chart.
 # Why 'base'? --> Because it will be shown on page all the time.
 # Below there is one bar chart that will be shown only on hover.
-fig = px.bar(
-    data_frame=df_grouped,
-    x='Order Date',
-    y='Profit',
-    template="simple_white"
-)
+fig = px.bar(data_frame=df_grouped, x="Order Date", y="Profit", template="simple_white")
 
 fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
 
@@ -44,16 +39,16 @@ fig.update_traces(hoverinfo="none", hovertemplate=None)
 
 app = Dash(__name__)
 
-app.layout = html.Div([
-    html.H4('Dash Tooltip Example', style={'textAlign' :'center'}),
-
-    # Base bar chart i.e. bar chart that will always be shown on map.
-    dcc.Graph(id="tooltip-graph-basic-2", figure=fig, clear_on_unhover=True),
-
-    # Component that will be updated when on hover.
-    # Here will come graph created in display_hover callback function.
-    dcc.Tooltip(id="tooltip-graph", direction='left'),
-])
+app.layout = html.Div(
+    [
+        html.H4("Dash Tooltip Example", style={"textAlign": "center"}),
+        # Base bar chart i.e. bar chart that will always be shown on map.
+        dcc.Graph(id="tooltip-graph-basic-2", figure=fig, clear_on_unhover=True),
+        # Component that will be updated when on hover.
+        # Here will come graph created in display_hover callback function.
+        dcc.Tooltip(id="tooltip-graph", direction="left"),
+    ]
+)
 
 
 @app.callback(
@@ -71,7 +66,7 @@ def display_hover(hover_data):
     # print(hover_data)
     # Here we are getting data about X-axis of hovered bar chart
     # i.e. we are interested in date of hovered bar.
-    x = hover_data['points'][0]['x']
+    x = hover_data["points"][0]["x"]
 
     # Here we are filtering first 7 digits of date value.
     # Why first 7? --> Because we want to have year and month.
@@ -82,16 +77,14 @@ def display_hover(hover_data):
     df_filtered = df.query(f"year_month == '{date_month}'")
 
     # Grouping data so that we have DataFrame grouped as per 'Segment' dimension.
-    df_filtered_grouped = df_filtered[['Segment', 'Profit']].groupby(by='Segment').sum().reset_index()
+    df_filtered_grouped = (
+        df_filtered[["Segment", "Profit"]].groupby(by="Segment").sum().reset_index()
+    )
 
     # Creating Plotly figure that will be shown in tooltip
     fig1 = px.bar(
-        data_frame=df_filtered_grouped,
-        x='Segment',
-        y='Profit',
-        template="simple_white"
+        data_frame=df_filtered_grouped, x="Segment", y="Profit", template="simple_white"
     )
-
     fig1.update_layout(margin=dict(t=0, l=0, r=0, b=0))
 
     # Purpose of following two lines is to get info about bounding box i.e.
@@ -100,15 +93,12 @@ def display_hover(hover_data):
     bbox = pt["bbox"]
 
     children = [
-        html.Div([
-
-            html.H4(f"Year-Month: {date_month}"),
-            dcc.Graph(figure=fig1),
-
-        ], style={'width': '200px',
-                  'height': '200px',
-                  'whiteSpace': 'normal'
-                  }
+        html.Div(
+            [
+                html.H4(f"Year-Month: {date_month}"),
+                dcc.Graph(figure=fig1),
+            ],
+            style={"width": "200px", "height": "200px", "whiteSpace": "normal"},
         )
     ]
 
@@ -117,5 +107,3 @@ def display_hover(hover_data):
 
 if __name__ == "__main__":
     app.run_server(debug=True)
-
-
