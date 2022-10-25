75a204aa593a89697687cbe1a4dbd23288ba47f8
Adam <adamschroeder.m@gmail.com>
2022-10-24 22:51:12 -0400

    Update flames.py

--- pages/flames.py
+++ pages/flames.py
@@ -5,7 +5,7 @@ from lib.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="This application demonstrates the use of html.Img and dbc.Modal through the F.L.A.M.E.S theory. It has a top-bottom layout and a regular-callback.",
+    description="This application demonstrates the use of html.Img and dbc.Modal through the F.L.A.M.E game. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -13,14 +13,14 @@ filename = __name__.split("pages.")[1]
 notes = """
 
 #### Dash Components in App:  
-- [dcc input](https://dash.plotly.com/dash-core-components/input)
-- [Button](https://dash.plotly.com/dash-html-components/button)
-- [html img](https://dash.plotly.com/dash-html-components/img)
+- [Input](https://dash.plotly.com/dash-core-components/input)
+- [Img](https://dash.plotly.com/dash-html-components/img)
 - [Markdown](https://dash.plotly.com/dash-core-components/markdown)
 
 #### Community Components:
 Dash Bootstrap Components 
-- [dbc Modal](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/modal/)
+- [Modal](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/modal/)
+- [Button](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/button/)
 
 #### Contributed by:
 This example app was contributed by [Someshfengde](https://github.com/someshfengde)
