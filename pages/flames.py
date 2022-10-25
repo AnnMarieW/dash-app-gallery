11f0b7e049a79a0a9e823dfc595a6cc79df79a25
Adam <adamschroeder.m@gmail.com>
2022-10-24 22:39:36 -0400

    Update flames.py

--- pages/flames.py
+++ pages/flames.py
@@ -5,8 +5,7 @@ from lib.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    order=0,
-    description="This application demonstrates the use of Img and dbc Modal. It has a top-bottom layout and a regular-callback and modal container.",
+    description="This application demonstrates the use of html.Img and dbc.Modal through the F.L.A.M.E.S theory. It has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -19,11 +18,12 @@ notes = """
 - [html img](https://dash.plotly.com/dash-html-components/img)
 - [Markdown](https://dash.plotly.com/dash-core-components/markdown)
 
-#### Dash Bootstrap Components:
+#### Community Components:
+Dash Bootstrap Components 
 - [dbc Modal](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/modal/)
 
 #### Contributed by:
-This example app was contributed by [Plotly](https://plotly.com/python/)
+This example app was contributed by [Someshfengde](https://github.com/someshfengde)
 
 """
 
