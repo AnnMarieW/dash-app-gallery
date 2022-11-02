2baff03427d63d866e3c65e81c3052315a5cc37a
AsafB <asafbm95@gmail.com>
2022-11-03 00:48:09 +0200

    add some style

--- examples/timeline-gantt.py
+++ examples/timeline-gantt.py
@@ -1,19 +1,18 @@
-import pandas
 import pandas as pd
 import plotly.figure_factory
 from dash import Dash, html, dcc, Input, Output, dash_table, ctx
+import dash_bootstrap_components as dbc
 
-APP_TOP_MARGIN = {
-    'text-align': 'center'}
 
-DATA_TABLE_SCHEMA = [{
-    'id': 'Task',
-    'name': 'Task',
-    'type': 'text'
-}, {
-    'id': 'Start',
-    'name': 'Start time of task',
-    'type': 'datetime'},
+DATA_TABLE_SCHEMA = [
+    {
+        'id': 'Task',
+        'name': 'Task',
+        'type': 'text'
+    }, {
+        'id': 'Start',
+        'name': 'Start time of task',
+        'type': 'datetime'},
     {
         'id': 'Duration',
         'name': 'Duration of task',
@@ -22,10 +21,11 @@ DATA_TABLE_SCHEMA = [{
         'id': 'Resource',
         'name': 'Resource',
         'type': 'text'
-    }]
+    }
+]
 
 
-def add_finish_column(df: pandas.DataFrame) -> pandas.DataFrame:
+def add_finish_column(df: pd.DataFrame) -> pd.DataFrame:
     """
     This function is used for creating 'Finish' column which is a required column for gantt chart.
     """
@@ -37,7 +37,7 @@ def add_finish_column(df: pandas.DataFrame) -> pandas.DataFrame:
 
 
 # Make data as a dataframe
-def get_default_csv_file() -> pandas.DataFrame:
+def get_default_table() -> pd.DataFrame:
     """
     This function import the csv file into a DataFrame.
     The dataset located in https://github.com/plotly/datasets/blob/master/GanttChart.csv
@@ -47,24 +47,31 @@ def get_default_csv_file() -> pandas.DataFrame:
     return add_finish_column(df)
 
 
-app = Dash(__name__, suppress_callback_exceptions=True)
-app.title = 'Time Chart App'
+app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB], suppress_callback_exceptions=True)
 
-new_order_line = {"Task": "", "Start": '2016-01-01', "Duration": 0, "Resource": "", "Finish": '2016-01-01'}
-df_new_order_line = pd.DataFrame(new_order_line, index=[0])
+new_task_line = {"Task": "", "Start": '2016-01-01', "Duration": 0, "Resource": "", "Finish": '2016-01-01'}
+df_new_task_line = pd.DataFrame(new_task_line, index=[0])
 
 app.layout = html.Div(
     [
-        html.H1("Project TimeLine", className="bg-primary text-white p-1", style=APP_TOP_MARGIN),
-        html.Button("+", n_clicks=0, id="add-btn"),
+        html.H1("Project TimeLine", className="bg-primary text-white p-1 text-center"),
         dash_table.DataTable(
             id="user-datatable",
             sort_action="native",
             columns=DATA_TABLE_SCHEMA,
             editable=True,
             row_deletable=True,
+            style_data_conditional=[{'backgroundColor': 'bg-primary'},
+                                    ],
+            style_header={
+                'backgroundColor': 'bg-primary bg-gradient text-white py-4',
+                'fontWeight': 'bold'
+            },
+
         ),
-        html.H3("Project Time Chart", style=APP_TOP_MARGIN),
+        html.Button("Add Row", n_clicks=0, id="add-btn"),  # if user want to add a row
+
+        html.H3("Project Time Chart", className="bg-light text-blue p-1 text-center"),
         dcc.Graph(id="graph"),
     ],
 )
@@ -77,25 +84,30 @@ app.layout = html.Div(
     Input("add-btn", "n_clicks"),
 )
 def update_table_and_figure(user_datatable: None or list, n_clicks) -> (list, dict):
+    """
+    This callback function returns both DataTable and Gantt Chart.
+    According to the user action a row can be added, restored the default table or just update the content of the
+    datatable (which will also update the gantt chart).
+    """
     # initialize table when app starts
     if user_datatable is None:
-        updated_table = get_default_csv_file()
+        updated_table = get_default_table()
 
-    # if user deleted all rows, add a default row:
+    # if user deleted all rows, return the default table:
     elif not user_datatable:
-        updated_table = get_default_csv_file()
+        updated_table = get_default_table()
 
-    # convert table data to DataFrame
+    # just convert table data to DataFrame
     else:
         updated_table = pd.DataFrame(user_datatable)
 
     # add a row
     if ctx.triggered_id == "add-btn":
         updated_table = pd.concat(
-            [pd.DataFrame(user_datatable), pd.DataFrame.from_records(df_new_order_line)]
+            [pd.DataFrame(user_datatable), pd.DataFrame.from_records(df_new_task_line)]
         )
 
-    updated_table_as_df = add_finish_column(updated_table)
+    updated_table_as_df = add_finish_column(updated_table)  # create the 'Finish' column for the gantt chart
 
     fig = plotly.figure_factory.create_gantt(
         updated_table_as_df,
