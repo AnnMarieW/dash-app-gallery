1cf07d9006b336ec1f27d2039bb6a309eccf88a9
AsafB <asafbm95@gmail.com>
2022-11-05 20:50:40 +0200

    Fixed: colors limit issue, app.layout to be in dbc.Container and not html,Div. Improved: code structure by adding functions for each action

--- examples/timeline-gantt.py
+++ examples/timeline-gantt.py
@@ -2,6 +2,7 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.figure_factory
 from dash import Dash, html, dcc, Input, Output, dash_table, ctx
+import plotly.express as px
 
 DATA_TABLE_SCHEMA = [
     {
@@ -28,8 +29,15 @@ DATA_TABLE_SCHEMA = [
     },
 ]
 
+DATA_TABLE_STYLE = {
+    'style_data_conditional': [{'backgroundColor': '#EEF2F7', 'maxWidth': '60%'},
+                               {"if": {"column_id": "Finish"}, "backgroundColor": "#eee"}],
+    'style_header': {'color': 'white', 'backgroundColor': '#799DBF', 'fontWeight': 'bold'},
+    'style_table': {'width': '93%', 'marginLeft': '3%', "overflowX": "auto"},
+}
 
-def add_finish_column(df: pd.DataFrame) -> pd.DataFrame:
+
+def __add_finish_column(df: pd.DataFrame) -> pd.DataFrame:
     """
     This function is used for creating 'Finish' column which is a required column for gantt chart.
     """
@@ -38,111 +46,104 @@ def add_finish_column(df: pd.DataFrame) -> pd.DataFrame:
     df['Finish'] = df['Start'] + pd.to_timedelta(df['Duration'], unit='D')
     df['Start'] = pd.to_datetime(df['Start']).dt.date
     df['Finish'] = pd.to_datetime(df['Finish']).dt.date
-
     return df
 
 
 # Make data as a dataframe
-def get_default_table() -> pd.DataFrame:
+def __get_default_table() -> pd.DataFrame:
     """
     This function import the csv file into a DataFrame.
     The dataset located in https://github.com/plotly/datasets/blob/master/GanttChart.csv
     """
 
     df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/GanttChart.csv')
-    return add_finish_column(df)
+    return __add_finish_column(df)
 
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.SPACELAB], suppress_callback_exceptions=True)
 
-new_task_line = {"Task": "", "Start": '2016-01-01', "Duration": 0, "Resource": "", "Finish": '2016-01-01'}
+new_task_line = {"Task": "Task 1", "Start": '2016-01-01', "Duration": 0, "Resource": "A", "Finish": '2016-01-01'}
 df_new_task_line = pd.DataFrame(new_task_line, index=[0])
 
-app.layout = html.Div(
+app.layout = dbc.Container(
     [
-        html.H3("Project Time Line", className="bg-primary text-white p-1 text-center"),
+        html.H1("Project Time Line", className="bg-primary text-white p-1 text-center"),
         dash_table.DataTable(
             id="user-datatable",
             sort_action="native",
             columns=DATA_TABLE_SCHEMA,
             editable=True,
             row_deletable=True,
-            style_data_conditional=[
-                {'backgroundColor': '#EEF2F7',
-                 'maxWidth': '60%'},
-            ],
-            style_header={
-                'color': 'white',
-                'backgroundColor': '#799DBF',
-                'fontWeight': 'bold'
-            },
-            style_table={'width': '93%',
-                         'marginLeft': '3%',
-                         },
+            style_data_conditional=DATA_TABLE_STYLE.get('style_data_conditional'),
+            style_header=DATA_TABLE_STYLE.get('style_header'),
+            style_table=DATA_TABLE_STYLE.get('style_table'),
         ),
 
-        html.Button("+", n_clicks=0, id="add-btn", style={'marginLeft': '3%'
-                                                          }),  # if user want to add a row
+        dbc.Button("+", n_clicks=0, id="add-row-btn", style={'marginLeft': '3%'}),  # if user want to add a row
+
         html.Hr(),
-        dcc.Graph(id="graph"),
-    ], style={'backgroundColor': '#DDE6EF'}
+        dcc.Graph(id="gantt-graph"),
+    ],
+    fluid=True,
+    style={'backgroundColor': '#DDE6EF'}
 )
 
 
-@app.callback(
-    Output("user-datatable", "data"),
-    Output("graph", "figure"),
-    Input("user-datatable", "derived_virtual_data"),
-    Input("add-btn", "n_clicks"),
-)
-def update_table_and_figure(user_datatable: None or list, n_clicks) -> (list, dict):
-    """
-    This callback function returns both DataTable and Gantt Chart.
-    According to the user action a row can be added, restored the default table or just update the content of the
-    datatable (which will also update the gantt chart).
-    """
+def update_datatable(user_datatable) -> pd.DataFrame:
+
     # initialize table when app starts
     if user_datatable is None:
-        updated_table = get_default_table()
+        updated_table = __get_default_table()
 
     # if user deleted all rows, return the default table:
-    elif not user_datatable:
-        updated_table = get_default_table()
-
-    # just convert table data to DataFrame
-    else:
-        updated_table = pd.DataFrame(user_datatable)
+    elif user_datatable==[]:  # @TODO: FIX BUG HERE DON"T WORK GOOD, The application is starting with this output and not with the default df
+        updated_table = pd.DataFrame.from_records(df_new_task_line)
 
     # add a row
-    if ctx.triggered_id == "add-btn":
+    elif ctx.triggered_id == "add-row-btn":
         updated_table = pd.concat(
-            [pd.DataFrame(user_datatable), pd.DataFrame.from_records(df_new_task_line)]
-        )
+            [pd.DataFrame(user_datatable), pd.DataFrame.from_records(df_new_task_line)])
+    # update datatable
+    else:
+        updated_table = pd.DataFrame(user_datatable)
+    return __add_finish_column(updated_table)  # create the 'Finish' column for the gantt chart
 
-    updated_table_as_df = add_finish_column(updated_table)  # create the 'Finish' column for the gantt chart
 
-    fig = plotly.figure_factory.create_gantt(
+def create_gantt_chart(updated_table_as_df):
+    gantt_fig = plotly.figure_factory.create_gantt(  # Cresting the gantt chart
         updated_table_as_df,
         index_col="Resource",
+        colors=px.colors.qualitative.Alphabet,
         show_colorbar=True,
         group_tasks=True,
         showgrid_x=False,
         showgrid_y=True,
         bar_width=0.5,
-
+        title='Project Plan Gantt Chart'
     )
-    fig.update_layout(
-        title='Project Plan Gantt Chart',
+    gantt_fig.update_layout(
         paper_bgcolor="#DDE6EF",
         title_x=0.5,
         font=dict(
             size=20,
-            color="#104870",
-        )
+            color="#104870")
+    ),
+    return gantt_fig
 
-    )
 
-    return updated_table_as_df.to_dict("records"), fig
+@app.callback(
+    Output("user-datatable", "data"),
+    Output("gantt-graph", "figure"),
+    Input("user-datatable", "derived_virtual_data"),
+    Input("add-row-btn", "n_clicks"),
+)
+def update_table_and_figure(user_datatable: None or list, n_clicks) -> (list, dict, dict):
+    """
+    This callback function returns both DataTable and Gantt Chart.
+    """
+    updated_table_as_df = update_datatable(user_datatable)
+    gantt_chart = create_gantt_chart(updated_table_as_df)
+    return updated_table_as_df.to_dict("records"), gantt_chart
 
 
 if __name__ == "__main__":
