7b2cecbbabfbb350b2790064f0dfbaa487541cfa
Tuomas Poukkula <66461922+tuopouk@users.noreply.github.com>
2022-11-05 15:56:10 +0200

    Update keyfiguresfinland.py
    I changed the whole generation of the map to the clientside. Now the app works really fast.

--- examples/keyfiguresfinland.py
+++ examples/keyfiguresfinland.py
@@ -4,10 +4,9 @@ from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import orjson
-from plotly.io.json import to_json_plotly
 
 # Data provided by Statistics Finland.
-regions_data = pd.read_csv('./assets/key_figures_regions.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Region'}).set_index('Region')
+regions_data = pd.read_csv('assets/key_figures_regions.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Region'}).set_index('Region')
 
 # Whole country figures on a pandas Series.
 whole_country_df = regions_data.loc['WHOLE COUNTRY']
@@ -17,7 +16,7 @@ regions_data.drop('WHOLE COUNTRY', axis = 0, inplace = True)
 
 # The json file for the mapbox viz.
 # https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=maakunta1000k_2021&outputFormat=json
-with open('./assets/regions.json', encoding = 'ISO-8859-1') as f:
+with open('assets/regions.json', encoding = 'ISO-8859-1') as f:
     regions_json = orjson.loads(f.read())
 
 key_figures = sorted(list(pd.unique(regions_data.columns)))
@@ -27,31 +26,6 @@ external_stylesheets = [dbc.themes.SUPERHERO]
 app = Dash(name = __name__, external_stylesheets = external_stylesheets)
 app.title = "Finland's Regional Key Figures"
 server = app.server
-
-
-# Empty map for initialization.
-def plot_empty_map():
-    return px.choropleth_mapbox(center = {"lat": 64.961093, "lon": 27.590605})
-
-# Generate map data and layout.
-def plot_map(key_figure):
-
-    fig = px.choropleth_mapbox(regions_data[[key_figure]].reset_index(), 
-                           geojson=regions_json, 
-                           locations='Region', 
-                           color=key_figure,
-                           mapbox_style="open-street-map",
-                           featureidkey='properties.name',
-                           zoom=4.2, 
-                           color_continuous_scale = 'viridis',
-                           center = {"lat": 64.961093, "lon": 27.590605},                           
-                           labels={key_figure: key_figure}
-                      )
-    fig = fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0},
-                      height=800,
-                      hoverlabel = dict(font_size = 28, font_family = 'Arial')
-                      )    
-    return fig
 
 
 app.layout = dbc.Container([
@@ -75,17 +49,16 @@ app.layout = dbc.Container([
                 ], xs = 12, sm = 12, md = 12, lg = 6, xl = 6, xxl = 6, align = 'center'),
             dbc.Col([
                 html.H1(id = 'key-figures-finland-header-x', style = {'textAlign':'center'}, className="mb-3 mt-3 display-3"),
-                dcc.Graph(id = 'key-figures-finland-region-map-x', figure = plot_empty_map())
+                dcc.Graph(id = 'key-figures-finland-region-map-x', figure = px.choropleth_mapbox(center = {"lat": 64.961093, "lon": 27.590605}))
                 
                 ], xs = 12, sm = 12, md = 12, lg = 6, xl = 6, xxl = 6)
 
             ], justify = 'center', className = "m-auto d-flex justify-content-center"),
-    
-        dcc.Store(id = 'key-figures-finland-map-data-store-x'), # Store map data.
-        dcc.Store(id = 'key-figures-finland-map-layout-store-x') # Store map layout.       
+  
+        dcc.Store(id = 'key-figures-finland-locations-x'), # Store locations for the map.
+        dcc.Store(id = 'key-figures-finland-zs-x'), # Store color (z variable) for the map.
+        dcc.Store(id = 'key-figures-finland-geojson-data', data = regions_json), # Store geojson for clientside callback.
         ], fluid = True)    
-
-
 
 @app.callback(Output('key-figures-finland-header-x','children'),Input('key-figures-finland-key-figure-selection-x', 'value') )
 def update_header(key_figure):
@@ -98,36 +71,54 @@ def update_whole_country_header(key_figure):
     kf_string = ', '.join(kf_string)
     kf_string = {True:key_figure, False:kf_string}[len(kf_string.split(','))==1]
     return html.P([kf_string,html.Br(),"in Finland:",html.Br(), ("{:,}".format(whole_country_df.loc[key_figure])).replace('.0','').replace(',',' ')+key_figure.split(',')[-2].replace('2020','').replace('2021','').replace(key_figure.split(',')[0],'')],className="fw-bold")
+ 
 
 @app.callback(
-    Output('key-figures-finland-map-data-store-x','data'),
-    Output('key-figures-finland-map-layout-store-x','data'),
-    Input('key-figures-finland-key-figure-selection-x', 'value')    
+
+    Output('key-figures-finland-locations-x','data'),
+    Output('key-figures-finland-zs-x','data'),
+    Input('key-figures-finland-key-figure-selection-x','value')   
+    
 )
-def update_map(key_figure):
-    
-    map_figure = plot_map(key_figure)        
-    map_data = orjson.loads(to_json_plotly(map_figure))['data']
-    map_layout = orjson.loads(to_json_plotly(map_figure))['layout']
-    
-    return map_data, map_layout    
+def store_data(key_figure):
+    df = regions_data[key_figure]
+    return list(df.index), list(df.values)
 
 # Update map on clientside.
 app.clientside_callback(
 
-    """
-    function(data, layout){    
-        return {
-            'data':data,
-            'layout':layout
-        }
-    }    
-    
-    """,
-    Output('key-figures-finland-region-map-x', 'figure'),
-    [Input('key-figures-finland-map-data-store-x','data'),
-    Input('key-figures-finland-map-layout-store-x','data')]
+"""
+    function(geojson, locations, z){           
+       
+        var layout = {
+            'height':800,
+            'mapbox': {'style':'open-street-map','zoom':4.2,'center':{'lat': 64.961093, 'lon': 27.590605}
+            },
+            'margin':{'l':0,'t':0,'b':0,'r':0
+            }
+        };
+        var data = [{            
+            'type':'choroplethmapbox',            
+            'name':'',
+            'subplot': 'mapbox',
+            'geojson':geojson,
+            'locations':locations,
+            'featureidkey':'properties.name',
+            'hovertemplate': '<b>%{location}</b><br>%{z:,}',
+            'hoverlabel':{'font':{'family':'Arial Black', 'size':20, 'color':'black'},'bgcolor':'white'},
+            'z':z,
+            'colorscale':'Viridis'
+        }];
+
+        return {'data':data,'layout':layout}
+    }   
+
+""",
+Output('key-figures-finland-region-map-x', 'figure'),
+Input('key-figures-finland-geojson-data','data'),
+Input('key-figures-finland-locations-x','data'),
+Input('key-figures-finland-zs-x','data')    
 )
 
 if __name__ == "__main__":
-    app.run_server(debug=False)   
+    app.run_server(debug=False)    
