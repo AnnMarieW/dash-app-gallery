78d9b6b65bbd3f20dfbd9a568a21fd27b7918609
Tuomas Poukkula <66461922+tuopouk@users.noreply.github.com>
2022-11-09 13:07:44 +0200

    Update keyfiguresfinland.py
    I made some changes: 
    - Moved the extraction of header fields to the beginning.
    - Simplified the header update callback.
    - Added 'white-space': 'nowrap' to the dropdown style to prevent overlapping on mobile

--- examples/keyfiguresfinland.py
+++ examples/keyfiguresfinland.py
@@ -6,17 +6,23 @@ import plotly.express as px
 import orjson
 
 # Data provided by Statistics Finland.
-regions_data = pd.read_csv('./assets/key_figures_regions.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Region'}).set_index('Region')
+# Original data source: https://pxdata.stat.fi/PxWeb/pxweb/en/Kuntien_avainluvut/Kuntien_avainluvut__2021/kuntien_avainluvut_2021_viimeisin.px/
+regions_data = pd.read_csv('assets/key_figures_regions.csv', encoding = 'latin-1').rename(columns ={'Region 2021':'Region'}).set_index('Region')
 
-# Whole country figures on a pandas Series.
-whole_country_df = regions_data.loc['WHOLE COUNTRY']
+# Whole country figures on a pandas Dataframe.
+# Extract information from the key figure names.
+whole_country_df = pd.DataFrame(regions_data.loc['WHOLE COUNTRY'])
+whole_country_df = whole_country_df.rename(columns = {'WHOLE COUNTRY':'value'})
+whole_country_df['year'] = [stat.split(', ')[-1] for stat in whole_country_df.index]
+whole_country_df['unit'] = [stat.split(', ')[-2] if len(stat.split(', ')) > 2 else '' for stat in whole_country_df.index]
+whole_country_df['stat_name'] = [stat.split(', ')[0] if len(stat.split(', ')) < 4 else ', '.join(stat.split(', ')[:2]) for stat in whole_country_df.index]
 
 # Drop since not needed.
 regions_data.drop('WHOLE COUNTRY', axis = 0, inplace = True)
 
 # The json file for the mapbox viz.
 # https://geo.stat.fi/geoserver/wfs?service=WFS&version=2.0.0&request=GetFeature&typeName=maakunta1000k_2021&outputFormat=json
-with open('./assets/regions.json', encoding = 'ISO-8859-1') as f:
+with open('assets/regions.json', encoding = 'ISO-8859-1') as f:
     regions_json = orjson.loads(f.read())
 
 key_figures = sorted(list(pd.unique(regions_data.columns)))
@@ -40,13 +46,14 @@ app.layout = dbc.Container([
                              options = [{'label':kf, 'value':kf} for kf in key_figures],
                              value = "Degree of urbanisation, %, 2020",
                              multi = False,
-                             style = {'font-size':20, 'font-family':'Arial','color': 'black'}
+                             style = {'font-size':'1.2rem', 'font-family':'Arial','color': 'black', 'white-space': 'nowrap'}
                              ),
                 html.Br(),
                 html.H1(id = 'key-figures-finland-whole-country-header-x', style = {'textAlign':'center'}, className="mt-5 display-2"),
                 html.Div(['Data by ',html.A('Statistics Finland', href = 'https://pxdata.stat.fi/PxWeb/pxweb/en/Kuntien_avainluvut/Kuntien_avainluvut__2021/kuntien_avainluvut_2021_viimeisin.px/', target = '_blank')], className="text-center fs-3 text"),
                 
                 ], xs = 12, sm = 12, md = 12, lg = 6, xl = 6, xxl = 6, align = 'center'),
+                
             dbc.Col([
                 html.H1(id = 'key-figures-finland-header-x', style = {'textAlign':'center'}, className="mb-3 mt-3 display-3"),
                 dcc.Graph(id = 'key-figures-finland-region-map-x', figure = px.choropleth_mapbox(center = {"lat": 64.961093, "lon": 27.590605}))
@@ -55,9 +62,9 @@ app.layout = dbc.Container([
 
             ], justify = 'center', className = "m-auto d-flex justify-content-center"),
   
-        dcc.Store(id = 'key-figures-finland-locations-x'), # Store locations for the map.
-        dcc.Store(id = 'key-figures-finland-zs-x'), # Store color (z variable) for the map.
-        dcc.Store(id = 'key-figures-finland-geojson-data', data = regions_json), # Store geojson for clientside callback.
+        dcc.Store(id = 'key-figures-finland-locations-x'),
+        dcc.Store(id = 'key-figures-finland-zs-x'),
+        dcc.Store(id = 'key-figures-finland-geojson-data', data = regions_json),
         ], fluid = True)    
 
 @app.callback(Output('key-figures-finland-header-x','children'),Input('key-figures-finland-key-figure-selection-x', 'value') )
@@ -66,12 +73,25 @@ def update_header(key_figure):
 
 @app.callback(Output('key-figures-finland-whole-country-header-x','children'),Input('key-figures-finland-key-figure-selection-x', 'value'))
 def update_whole_country_header(key_figure):
-    kf_string = key_figure.split(',')
-    kf_string.pop(-2)
-    kf_string = ', '.join(kf_string)
-    kf_string = {True:key_figure, False:kf_string}[len(kf_string.split(','))==1]
-    return html.P([kf_string,html.Br(),"in Finland:",html.Br(), ("{:,}".format(whole_country_df.loc[key_figure])).replace('.0','').replace(',',' ')+key_figure.split(',')[-2].replace('2020','').replace('2021','').replace(key_figure.split(',')[0],'')],className="fw-bold")
+    
+    # Get all the header components.
+    stat_name = whole_country_df.loc[key_figure].stat_name
+    stat_unit = whole_country_df.loc[key_figure].unit
+    stat_year = whole_country_df.loc[key_figure].year
+    stat_value = whole_country_df.loc[key_figure].value
  
+    # Change values with no decimals (.0) to int.
+    stat_value = {True: int(stat_value), False: stat_value}['.0' in str(stat_value)]
+    # Use space as thousand separator.
+    stat_value = "{:,}".format(stat_value).replace(',',' ')
+    
+    return html.Div(
+        [
+            html.Div([stat_name, ", ", stat_year]),
+            html.Div("in Finland"),
+            html.Span([stat_value, " ", stat_unit]),
+        ]
+    )
 
 @app.callback(
 
