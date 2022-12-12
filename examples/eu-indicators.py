571e7abd9697915612773bc3927610114439d7f2
Tuomas Poukkula <66461922+tuopouk@users.noreply.github.com>
2022-12-12 10:50:46 +0200

    Update eu-indicators.py
    I added a comment on the title text and also modified the code so the country dropdown does not use dcc.Store in between.

--- examples/eu-indicators.py
+++ examples/eu-indicators.py
@@ -5,22 +5,23 @@ import dash_bootstrap_components as dbc
 import plotly.graph_objs as go
 from io import StringIO
 
+# This a link to the web interface where the data can be downloaded.
 web_url = "https://sotkanet.fi/sotkanet/en/taulukko/?indicator=LclBCoAwEAPAF6WwWymFnMVnBBXrST0oCL7eFbyESUI_D00XsRCbyj0QVmX0hEJU5mTKhMdklVjjVrPIrJ0Yv-odkeVBoqmffxFP4AU=&region=s05zsy7TM4w3sjYCkvnWqXqGAA==&year=sy5ztk7V0zUEAA==&gender=m;f;t&abs=f&color=f&buildVersion=3.1.1&buildTimestamp=202211091024"
 
+# This is the download link. In order to read it in pandas, one needs to convert it into StringIO first.
 file_url = 'https://sotkanet.fi/sotkanet/en/csv?indicator=LclBCoAwEAPAF6WwWymFnMVnBBXrST0oCL7eFbyESUI_D00XsRCbyj0QVmX0hEJU5mTKhMdklVjjVrPIrJ0Yv-odkeVBoqmffxFP4AU=&region=s05zsy7TM4w3sjYCkvnWqXqGAA==&year=sy5ztk7V0zUEAA==&gender=m;f;t&abs=f&color=f&buildVersion=3.1.1&buildTimestamp=202211091024&order=G'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
  'Content-Type': 'text/html; charset=utf-8'}
 
 bytes_data = requests.get(file_url,headers=headers).content
-s=str(bytes_data,'utf-8')
+s = str(bytes_data,'utf-8')
 data = StringIO(s) 
-df=pd.read_csv(data,sep=';', header=None)   
+df = pd.read_csv(data,sep=';', header=None)   
 df = df.set_index(list(df.columns[:-2])).dropna(axis=0, how='all').reset_index()
 df.drop([1,3,7],axis=1, inplace = True)
 df.columns = ['Indicator','Country', 'Gender','Year','Val']
 df.Val = df.Val.str.replace(',','.').astype(float)
-df.Year = df.Year.astype(str)
 
 indicators = sorted(list(df.Indicator.unique()))
 countries = sorted(list(df.Country.unique()))
@@ -72,7 +73,6 @@ app.layout = dbc.Container(
             ],
             style={"textAlign": "center"},
         ),
-        dcc.Store(id="eu-indicators-selected-indicator-x"),
         html.Div(id="eu-indicators-printing-hidden-content-x"),
     ]
 )
@@ -88,48 +88,63 @@ app.clientside_callback(
     Output("eu-indicators-printing-hidden-content-x", "children"),
     Input("eu-indicators-printing-x", "n_clicks"),
 )
-# Update countries dropdown based on indicator selected
-@app.callback(
-    Output("eu-indicators-country-dropdown-x", "options"),
-    Output("eu-indicators-country-dropdown-x", "value"),
-    Output("eu-indicators-selected-indicator-x", "data"),
-    Output("eu-indicators-available-countries-x", "children"),
-    Input("eu-indicators-indicator-dropdown-x", "value"),
-)
-def update_country_dropdown(indicator):
-    dff = df[df.Indicator == indicator].dropna(axis=0)
 
-    country_list = sorted(list(dff.Country.unique()))
-    return country_list, country_list[0], indicator, f"#### Available countries: {len(country_list)} / {len(countries)}",
-
-
-# Create the Graph
 @app.callback(
     Output("eu-indicators-graph-x", "children"),
-    Input("eu-indicators-selected-indicator-x", "data"),
+    Input("eu-indicators-indicator-dropdown-x", "value"),
     Input("eu-indicators-country-dropdown-x", "value")
 )
 def update_indicator_graph(indicator, country):
+
     dff = df[(df.Indicator == indicator) & (df.Country == country)].dropna(axis=0)
 
+    # In some cases there is only data for one year, so this line prevents notations such as '2016 - 2016'.  
     title_text = {False: f"{dff.Year.min()} - {dff.Year.max()}", True: str(dff.Year.min())}[
         dff.Year.min() == dff.Year.max()
     ]
 
-    # if data has only one year create a bar chart, otherwise a scatter plot
-    if dff['Year'].nunique() == 1:
-        fig = px.bar(dff, x='Year', y='Val', color='Gender', barmode='group', height=600)
-        fig.update_layout(title=dict(text=f"{indicator} in {country}<br>{title_text} by gender", x=0.5),
-                          yaxis=dict(title=indicator, tickformat =' '),
-                          xaxis=dict(title="Years"))
-    else:
-        fig = px.line(dff, x='Year', y='Val', color='Gender', height=600)
-        fig.update_layout(title=dict(text=f"{indicator} in {country}<br>{title_text} by gender", x=0.5),
-                          yaxis=dict(title=indicator, tickformat =' '),
-                          xaxis=dict(title="Years"))
+    traces = []
+    for gender in dff.Gender.unique():
 
-    return dcc.Graph(figure=fig)
+        dfff = dff[dff.Gender == gender]
 
+
+        if len(dfff) > 1:
+            traces.append(
+                go.Scatter(x=dfff.Year.astype(str), y=dfff.Val, name=gender, showlegend=True)
+            )
+        else:
+            traces.append(
+                go.Bar(x=dfff.Year.astype(str), y=dfff.Val,name=gender,showlegend=True)
+            )
+          
+    
+    return dcc.Graph(
+        figure=go.Figure(
+            data=traces,
+            layout=go.Layout(
+                title=dict(
+                    text=f"{indicator} in {country}<br>{title_text} by gender", x=0.5
+                ),
+                xaxis=dict(title="Years"),
+                yaxis=dict(title=indicator),
+                legend = dict(title = 'Gender'),
+                height = 600
+            ),
+        )
+    )
+@app.callback(
+    Output("eu-indicators-country-dropdown-x", "options"),
+    Output("eu-indicators-country-dropdown-x", "value"),
+    Output("eu-indicators-available-countries-x", "children"),
+    Input("eu-indicators-indicator-dropdown-x", "value"),
+)
+def update_country_dropdown(indicator):
+
+    dff = df[df.Indicator == indicator].dropna(axis=0)
+
+    country_list = sorted(list(dff.Country.unique()))
+    return country_list, country_list[0], f"#### Available countries: {len(country_list)} / {len(countries)}",
     
 if __name__ == "__main__":
     app.run_server(debug=True)
