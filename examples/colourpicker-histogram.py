9b07ebe284c232942d433c5945d0b55eba7ecb59
Jenna Le Noble <jenna_lenoble@hotmail.com>
2023-04-02 23:24:38 -0700

    updated fixes to the app example + format with black

--- examples/colourpicker-histogram.py
+++ examples/colourpicker-histogram.py
@@ -1,18 +1,19 @@
 from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 import dash_daq as daq
 import plotly.io as pio
+import time
 
 df = px.data.gapminder()
-df = df.loc[df['year']==1952]
+df = df.loc[df["year"] == 1952]
 
+# Set the template to ggplot2
+# Here are all the default templates:
+# "ggplot2", "seaborn", "simple_white", "plotly", "plotly_white", "plotly_dark", "presentation", "xgridoff", "ygridoff", "gridon", "none"
 pio.templates.default = "ggplot2"
 
 app = Dash(__name__)
-
-picker_style = {"display": "inline-block", "margin": 10}
 
 app.layout = html.Div(
     [
@@ -22,25 +23,29 @@ app.layout = html.Div(
             id="colourpicker-histogram-x-color",
             label="Color of Bars",
             size=164,
-            value=dict(hex="#564AE6")
+            value=dict(hex="#564AE6"),
         ),
     ]
 )
 
 
 @app.callback(
-    Output("colourpicker-histogram-x-graph", "figure"), Input("colourpicker-histogram-x-color", "value"),
+    Output("colourpicker-histogram-x-graph", "figure"),
+    Input("colourpicker-histogram-x-color", "value"),
 )
-
-
 def update_points_color(color):
-    fig = px.histogram(df, x='lifeExp',
-                       labels={'lifeExp': "Life Expectancy (years)"},
-                       title="Life Expectancy for Countries in 1952", nbins=23)
-    fig = go.Figure(fig)
+    fig = px.histogram(
+        df,
+        x="lifeExp",
+        labels={"lifeExp": "Life Expectancy (years)"},
+        title="Life Expectancy for Countries in 1952",
+        nbins=23,
+    )
     fig.update_layout(yaxis_title="Number of Countries")
-    fig.update_traces(marker=dict(
-        color=color["hex"]))
+    fig.update_traces(marker=dict(color=color["hex"]))
+    time.sleep(
+        2
+    )  # simulate longer callback to make loading component (spinner) more visible
     return fig
 
 
--- pages/colourpicker-histogram.py
+++ pages/colourpicker-histogram.py
@@ -5,7 +5,7 @@ from lib.code_and_show import example_app
 
 dash.register_page(
     __name__,
-    description="This app shows a histogram of life expectancy and uses ColourPicker to change the colour of the bars. This app has a top-bottom layout and a regular-callback."
+    description="This app shows a histogram of life expectancy and uses ColourPicker to change the colour of the bars. This app has a top-bottom layout and a regular-callback.",
 )
 
 filename = __name__.split("pages.")[1]
@@ -24,6 +24,5 @@ notes = """
 #### Contributed by:
 This example app was contributed by [Jenna Le Noble](https://github.com/jennalenoble)
 """
-
 
 layout = example_app(filename, notes=notes)
